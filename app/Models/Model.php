04bac0d76223c15e21d446726258030299c78abe
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-18 00:43:16 -0700

    Add accessors for created_at and updated_at columns
    Converts their values to \DateTime.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -7,6 +7,28 @@ use Illuminate\Support\Facades\Schema;
 
 class Model extends EloquentModel
 {
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(get: function ($value) {
+            if (gettype($value) === "string") {
+                return new \DateTime($value);
+            } elseif ($value instanceof Carbon) {
+                return new \DateTime($value->toDateTimeString());
+            }
+        });
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(get: function ($value) {
+            if (gettype($value) === "string") {
+                return new \DateTime($value);
+            } elseif ($value instanceof Carbon) {
+                return new \DateTime($value->toDateTimeString());
+            }
+        });
+    }
+
     /**
      * The purpose of this method is to get current values of all the visible attributes in a model instance even if they hsve null value.
      *
