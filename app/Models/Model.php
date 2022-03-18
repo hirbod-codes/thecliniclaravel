7d320151fbc6e817d22a37b88dcf612d034c1d3f
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-18 00:50:51 -0700

    Add getModelsFullnameInDirectory method to \App\Models\Model class
    gets models full names in a directory recursively.

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -2,11 +2,67 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 use Illuminate\Support\Facades\Schema;
 
 class Model extends EloquentModel
 {
+    public function getModelsFullnameInDirectory(string $dir): array
+    {
+        $modelFullnames = [];
+
+        $namespace = $this->resolveNamespace($dir);
+
+        foreach (scandir($dir) as $fileOrFolder) {
+            if (in_array($fileOrFolder, ['.', '..'])) {
+                continue;
+            }
+
+            if (is_dir($dir . '/' . $fileOrFolder)) {
+                array_push($modelFullnames, ...$this->{__FUNCTION__}($dir . '/' . $fileOrFolder));
+            }
+
+            $modelFullname = $namespace . class_basename(str_replace(['.php', '/'], ['', '\\'], $fileOrFolder));
+
+            if (self::class === $modelFullname) {
+                continue;
+            }
+
+            if (
+                class_exists($modelFullname) &&
+                ($reflectionParent = (new \ReflectionClass($modelFullname))->getParentClass()) !== false &&
+                in_array($reflectionParent->getName(), [EloquentModel::class, User::class])
+            ) {
+                $modelFullnames[] = $modelFullname;
+            }
+        }
+
+        return $modelFullnames;
+    }
+
+    private function resolveNamespace(string $dir): string
+    {
+        $previous = '';
+        $start = false;
+        $fragments = [];
+        foreach (explode('\\', str_replace('/', '\\', $dir)) as $item) {
+            if ($item === 'Models' && $previous === 'app') {
+                $start = true;
+                continue;
+            }
+            $previous = $item;
+
+            if (!$start) {
+                continue;
+            }
+
+            $fragments[] = $item;
+        }
+
+        return 'App\\Models\\' . (!empty($fragments) ? implode('\\', $fragments) . '\\' : '');
+    }
+
     protected function createdAt(): Attribute
     {
         return Attribute::make(get: function ($value) {
