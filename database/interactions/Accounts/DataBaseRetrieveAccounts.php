76329fb982cb44d84f39b519b5f32850ff77248d
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-18 19:13:29 -0700

    Create DataBaseRetrieveAccounts and DataBaseRetrieveAccountsTest classes

---
+++ database/interactions/Accounts/DataBaseRetrieveAccounts.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Interactions\Accounts;
+
+use Database\Traits\ResolveUserModel;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use TheClinicDataStructures\DataStructures\User\DSUser;
+use TheClinicUseCases\Accounts\Interfaces\IDataBaseRetrieveAccounts;
+
+class DataBaseRetrieveAccounts implements IDataBaseRetrieveAccounts
+{
+    use ResolveUserModel;
+
+    /**
+     * @param integer|null $lastAccountId
+     * @param integer $count
+     * @param string $ruleName
+     * @return \TheClinic\DataStructures\User\DSUser[]
+     */
+    public function getAccounts(?int $lastAccountId = null, int $count, string $ruleName): array
+    {
+        $theModelFullName = $this->resolveRuleModelFullName($ruleName);
+
+        $maxId = $theModelFullName::orderBy('id', 'desc')->first()->id;
+
+        if (!is_null($lastAccountId) && ($lastAccountId > $maxId || $lastAccountId < 1)) {
+            throw new \RuntimeException('Invalid last primary key value, it\'s either greater than max primary key or less than 1.', 500);
+        }
+
+        $models = $theModelFullName::orderBy((new $theModelFullName)->getKeyName(), 'desc')
+            ->where((new $theModelFullName)->getKeyName(), $lastAccountId === null ? '<=' : '<', $lastAccountId === null ? $maxId : $lastAccountId)
+            ->take($count)
+            ->get();
+
+        $authenticatables = [];
+        foreach ($models as $authenticatable) {
+            $authenticatables[] = $authenticatable->getDataStructure();
+        }
+
+        return $authenticatables;
+    }
+
+    public function getAccount(int $id, string $ruleName): DSUser
+    {
+        $theModelFullName = $this->resolveRuleModelFullName($ruleName);
+
+        if (($authenticatable = $theModelFullName::where('id', $id)->first()) === null) {
+            throw new ModelNotFoundException("The user not found.", 404);
+        }
+
+        return $authenticatable->getDataStructure();
+    }
+}
---
+++ tests/Unit/database/interactions/Accounts/DataBaseRetrieveAccountsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit\database\interaction\Accounts;
+
+use Database\Interactions\Accounts\DataBaseRetrieveAccounts;
+use Database\Traits\ResolveUserModel;
+use Faker\Factory;
+use Faker\Generator;
+use Tests\TestCase;
+use Tests\Unit\Traits\GetAuthenticatables;
+
+class DataBaseRetrieveAccountsTest extends TestCase
+{
+    use ResolveUserModel, GetAuthenticatables;
+
+    private Generator $faker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = Factory::create();
+    }
+
+    public function testGetAccounts()
+    {
+        $count = 5;
+
+        $ruleName = 'patient';
+
+        $theModelClassFullName = $this->resolveRuleModelFullName($ruleName);
+        $theModelClassPrimeryKey = (new $theModelClassFullName)->getKeyName();
+        $maxId = $theModelClassFullName::orderBy($theModelClassPrimeryKey, 'desc')->first()->{$theModelClassPrimeryKey};
+
+        $theDataStructureClassFullName = $this->resolveRuleDataStructureFullName($ruleName);
+
+        $lastAccountId = $this->faker->randomElement([
+            null,
+            $this->faker->numberBetween($count + 1, $maxId)
+        ]);
+
+        $accounts = (new DataBaseRetrieveAccounts)->getAccounts($lastAccountId, $count, $ruleName);
+
+        $this->assertIsArray($accounts);
+        $this->assertCount($count, $accounts);
+
+        for ($i = 0; $i < $count; $i++) {
+            $this->assertInstanceOf($theDataStructureClassFullName, $accounts[$i]);
+            $this->assertEquals(($lastAccountId ? $lastAccountId - 1 : $maxId) - $i, $accounts[$i]->getId());
+        }
+    }
+
+    public function testGetAccount()
+    {
+        $ruleName = 'patient';
+
+        $authenticatable = $this->getAuthenticatable($ruleName);
+
+        $theModelClassFullName = $this->resolveRuleModelFullName($ruleName);
+        $theModelClassPrimeryKey = (new $theModelClassFullName)->getKeyName();
+
+        $theDataStructureClassFullName = $this->resolveRuleDataStructureFullName($ruleName);
+
+        $account = (new DataBaseRetrieveAccounts)->getAccount($authenticatable->{$theModelClassPrimeryKey}, $ruleName);
+
+        $this->assertInstanceOf($theDataStructureClassFullName, $account);
+        $this->assertEquals($authenticatable->getDataStructure()->getUsername(), $account->getUsername());
+    }
+}
