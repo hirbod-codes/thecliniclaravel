f16bf238fd6bcdb4abbfc6b77f173ef9ca48bc48
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-25 23:38:24 -0700

    Fix role model names in GetAuthenticatables trait

--- tests/Unit/Traits/GetAuthenticatables.php
+++ tests/Unit/Traits/GetAuthenticatables.php
@@ -3,53 +3,48 @@
 namespace Tests\Unit\Traits;
 
 use App\Models\Auth\User as Authenticatable;
-use App\Models\rules\AdminRule;
-use App\Models\rules\DoctorRule;
-use App\Models\rules\OperatorRule;
-use App\Models\rules\PatientRule;
-use App\Models\rules\SecretaryRule;
+use App\Models\roles\AdminRole;
+use App\Models\roles\DoctorRole;
+use App\Models\roles\OperatorRole;
+use App\Models\roles\PatientRole;
+use App\Models\roles\SecretaryRole;
 use App\Models\User;
 
 trait GetAuthenticatables
 {
-    private function getAuthenticatable(string $ruleName): Authenticatable
+    private function getAuthenticatable(string $roleName): Authenticatable
     {
-        return $this->getAuthenticatables(true)[$ruleName];
+        return $this->getAuthenticatables(true)[$roleName];
     }
 
     private function getAuthenticatables(bool $randomId = false): array
     {
-        $adminRuleId = (new AdminRule)->getKeyName();
-        $doctorRuleId = (new DoctorRule)->getKeyName();
-        $secretaryRuleId = (new SecretaryRule)->getKeyName();
-        $operatorRuleId = (new OperatorRule)->getKeyName();
-        $patientRuleId = (new PatientRule())->getKeyName();
-        $userId = (new User)->getKeyName();
+        $adminRoleId = (new AdminRole)->getKeyName();
+        $doctorRoleId = (new DoctorRole)->getKeyName();
+        $secretaryRoleId = (new SecretaryRole)->getKeyName();
+        $operatorRoleId = (new OperatorRole)->getKeyName();
+        $patientRoleId = (new PatientRole())->getKeyName();
 
         return [
-            'admin' => AdminRule::where(
-                $adminRuleId,
-                $randomId ? $this->faker->numberBetween(2, AdminRule::orderBy($adminRuleId, 'desc')->first()->{$adminRuleId}) : 1
+            'admin' => AdminRole::where(
+                $adminRoleId,
+                $randomId ? $this->faker->numberBetween(2, AdminRole::orderBy($adminRoleId, 'desc')->first()->{$adminRoleId}) : 1
             )->first(),
-            'doctor' => DoctorRule::where(
-                $doctorRuleId,
-                $randomId ? $this->faker->numberBetween(2, DoctorRule::orderBy($doctorRuleId, 'desc')->first()->{$doctorRuleId}) : 1
+            'doctor' => DoctorRole::where(
+                $doctorRoleId,
+                $randomId ? $this->faker->numberBetween(2, DoctorRole::orderBy($doctorRoleId, 'desc')->first()->{$doctorRoleId}) : 1
             )->first(),
-            'secretary' => SecretaryRule::where(
-                $secretaryRuleId,
-                $randomId ? $this->faker->numberBetween(2, SecretaryRule::orderBy($secretaryRuleId, 'desc')->first()->{$secretaryRuleId}) : 1
+            'secretary' => SecretaryRole::where(
+                $secretaryRoleId,
+                $randomId ? $this->faker->numberBetween(2, SecretaryRole::orderBy($secretaryRoleId, 'desc')->first()->{$secretaryRoleId}) : 1
             )->first(),
-            'operator' => OperatorRule::where(
-                $operatorRuleId,
-                $randomId ? $this->faker->numberBetween(2, OperatorRule::orderBy($operatorRuleId, 'desc')->first()->{$operatorRuleId}) : 1
+            'operator' => OperatorRole::where(
+                $operatorRoleId,
+                $randomId ? $this->faker->numberBetween(2, OperatorRole::orderBy($operatorRoleId, 'desc')->first()->{$operatorRoleId}) : 1
             )->first(),
-            'patient' => PatientRule::where(
-                $patientRuleId,
-                $randomId ? $this->faker->numberBetween(2, PatientRule::orderBy($patientRuleId, 'desc')->first()->{$patientRuleId}) : 1
-            )->first(),
-            'custom' => User::where(
-                $userId,
-                $randomId ? $this->faker->numberBetween(2, User::orderBy($userId, 'desc')->first()->{$userId}) : 1
+            'patient' => PatientRole::where(
+                $patientRoleId,
+                $randomId ? $this->faker->numberBetween(2, PatientRole::orderBy($patientRoleId, 'desc')->first()->{$patientRoleId}) : 1
             )->first(),
         ];
     }
