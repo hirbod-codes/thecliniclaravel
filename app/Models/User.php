aba29ba714a634bf26eced745995a33b4400f598
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-25 23:34:01 -0700

    Add \App\Models\User::authenticatableRole method
    A custom relationship method that finds the current user's related role model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Models\Auth\User as Authenticatable;
 use App\Models\rules\DSCustom;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Schema;
 
 class User extends Authenticatable
 {
@@ -15,4 +16,21 @@ class User extends Authenticatable
     protected $table = "users";
 
     protected string $DS = DSCustom::class;
+
+    public function authenticatableRole(): Authenticatable|null
+    {
+        foreach ($allModelsFullnames = $this->getAllModelsFullname() as $modelFullname) {
+            if (array_search($this->getForeignKey(), Schema::getColumnListing((new $modelFullname)->getTable())) === false) {
+                continue;
+            }
+
+            if (($authenticatable = $this->hasOne($modelFullname, $this->getForeignKey(), $this->getKeyName())->first()) === null) {
+                continue;
+            }
+
+            return $authenticatable;
+        }
+
+        return null;
+    }
 }
