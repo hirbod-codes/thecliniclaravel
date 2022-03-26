19252a00a9d7affe6cc015421b2c3832fddd416f
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-25 23:25:01 -0700

    Add AuthController and AccountsController routes in ./routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,31 @@ Route::group(['middleware' => ['web', 'auth']], function ($router) {
     $router->delete('/clients/{client_id}', function () {
     });
 });
+
+$authMiddleware = 'auth:' . implode(', ', array_keys(app()['config']['auth.guards']));
+
+Route::controller(AccountsController::class)
+    ->middleware($authMiddleware)
+    ->group(function () {
+        Route::get('/accounts/ruleName/{count?}/{lastAccountId?}', 'index')->name('accounts.index');
+
+        Route::get('/accounts/{accountId}/privileges', 'privileges')->name('accounts.privileges');
+
+        // Route::get('/accounts/create', 'create')->name('accounts.create');
+
+        Route::post('/accounts', 'store')->name('accounts.store');
+
+        Route::get('/accounts/{accountId}', 'show')->name('accounts.show');
+
+        // Route::get('/accounts/{accountId}/edit', 'edit')->name('accounts.edit');
+
+        Route::put('/accounts/{accountId}', 'update')->name('accounts.update');
+
+        Route::delete('/accounts/{accountId}', 'destroy')->name('accounts.destroy');
+    });
+
+Route::controller(AuthController::class)->group(function () use ($authMiddleware) {
+    Route::middleware('guest')->put('/register', 'register')->name('auth.register');
+
+    Route::middleware($authMiddleware)->get('/logout', 'logout')->name('auth.logout');
+});
