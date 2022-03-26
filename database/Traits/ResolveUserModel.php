b8fa80427a8bcf0faa22459bff856e39e0814bf9
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-25 23:06:32 -0700

    Update ResolveUserModel trait
    Added resolveRuleFactoryFullName and 
    resolveRuleName methods.

--- database/Traits/ResolveUserModel.php
+++ database/Traits/ResolveUserModel.php
@@ -2,18 +2,29 @@
 
 namespace Database\Traits;
 
-use App\Models\rules\DSCustom;
+use App\Models\Model;
+use App\Models\roles\DSCustom;
 use App\Models\User;
+use TheClinicDataStructures\DataStructures\User\DSUser;
 
 trait ResolveUserModel
 {
+    public function resolveRuleName(DSUser|Model|string $modelFullname): string
+    {
+        if ($modelFullname instanceof DSUser) {
+            return str_replace('ds', '', strtolower(class_basename($modelFullname)));
+        } else {
+            return str_replace('role', '', strtolower(class_basename($modelFullname)));
+        }
+    }
+
     public function resolveRuleModelFullName(string $ruleName): string
     {
         if ($ruleName === 'custom') {
             return User::class;
         }
 
-        return 'App\\Models\\rules\\' . ucfirst($ruleName) . 'Rule';
+        return 'App\\Models\\roles\\' . ucfirst($ruleName) . 'Role';
     }
 
     public function resolveRuleDataStructureFullName(string $ruleName): string
@@ -24,5 +35,9 @@ trait ResolveUserModel
 
         return 'TheClinicDataStructures\\DataStructures\\User\\DS' . ucfirst($ruleName);
     }
-}
 
+    public function resolveRuleFactoryFullName(string $ruleName): string
+    {
+        return 'Database\\Factories\\roles\\' . ucfirst($ruleName) . 'RoleFactory';
+    }
+}
