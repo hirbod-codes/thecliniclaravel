b5d1bdc75d66a21eebc02a95601161d7e6293653
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-25 23:43:47 -0700

    Update CheckAuthentication class

--- app/Auth/CheckAuthentication.php
+++ app/Auth/CheckAuthentication.php
@@ -2,28 +2,55 @@
 
 namespace App\Auth;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use App\Models\Auth\User as Authenticatable;
+use App\Models\Model;
 use Illuminate\Support\Facades\Auth;
 use TheClinicDataStructures\DataStructures\User\DSUser;
 use TheClinicDataStructures\DataStructures\User\ICheckAuthentication;
+use TheClinicUseCases\Accounts\ICheckForAuthenticatedUsers;
 
-class CheckAuthentication implements ICheckAuthentication
+class CheckAuthentication implements
+    ICheckAuthentication,
+    ICheckForAuthenticatedUsers
 {
+    public function checkIfThereIsNoAuthenticated(): bool
+    {
+        return $this->getAuthenticated() === null;
+    }
+
     public function getAuthenticated(): Authenticatable|null
     {
-        return Auth::user();
+        foreach ($guards = app()['config']["auth.guards"] as $name => $guard) {
+            if (($user = Auth::guard($name)->user()) !== null) {
+                return $user;
+            }
+        }
+
+        return null;
     }
 
     public function getAuthenticatedDSUser(): DSUser
     {
         /** @var \App\Models\User $user */
-        $user = $this->getAuthenticated();
+        if (($user = $this->getAuthenticated()) === null) {
+            throw new \RuntimeException('There is no authenticated user.', 401);
+        }
 
         return $user->getDataStructure();
     }
 
     public function isAuthenticated(DSUser $user): bool
     {
-        return !is_null(Auth::user()) && isset(Auth::user()->id) && Auth::user()->id === $user->getId();
+        if (($authenticated = $this->getAuthenticated()) === null) {
+            return false;
+        }
+
+        if ($authenticated instanceof Model) {
+            $primaryKey = (new $authenticated)->getKeyName();
+        } else {
+            $primaryKey = 'id';
+        }
+
+        return !is_null($authenticated) && isset($authenticated->{$primaryKey}) && $authenticated->{$primaryKey} === $user->getId();
     }
 }
