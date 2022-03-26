294edfe317459a51ea73ae4b4fce733853a44d14
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-25 23:32:38 -0700

    Remove \App\Models\User::getDataStructure method

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use App\Models\Auth\User as Authenticatable;
 use App\Models\rules\DSCustom;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
-use TheClinicDataStructures\DataStructures\User\DSUser;
 
 class User extends Authenticatable
 {
@@ -16,9 +15,4 @@ class User extends Authenticatable
     protected $table = "users";
 
     protected string $DS = DSCustom::class;
-
-    public function getDataStructure(array $additionalArgs = []): DSUser
-    {
-        return parent::getDataStructure(array_merge($additionalArgs, ['ruleName' => $this->rule()->first()->name]));
-    }
 }
