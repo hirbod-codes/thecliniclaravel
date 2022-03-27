da2f583c8210fcd72caca70d6830504162d39be2
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-27 00:36:51 -0700

    Remove foreign key user_id and Add foreign key id on role tables
    id foreign key points to the id column of users table

--- database/migrations/TraitBaseUserRoleColumns.php
+++ database/migrations/TraitBaseUserRoleColumns.php
@@ -5,23 +5,26 @@ namespace Database\Migrations;
 use App\Models\Role;
 use App\Models\roles\AdminRole;
 use App\Models\User;
+use Database\Traits\ResolveUserModel;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 trait TraitBaseUserRoleColumns
 {
+    use ResolveUserModel;
+    
     public function createBaseUserRoleColumns(string $tableName, string $roleName): void
     {
         $fkUserRole = '';
-        Schema::create($tableName, function (BluePrint $table) use (&$fkUserRole, $tableName) {
-            $table->id();
+        Schema::create($tableName, function (BluePrint $table) use (&$fkUserRole, $roleName, $tableName) {
+            $modelFullname = $this->resolveRuleModelFullname($roleName);
 
-            $fkUser = (new User)->getForeignKey();
+            $table->id((new $modelFullname)->getKeyName());
+
             $userTable = (new User)->getTable();
 
-            $table->unsignedBigInteger($fkUser)->unique();
-            $table->foreign($fkUser, $tableName . '_' . $userTable . '_' . $fkUser)
+            $table->foreign((new $modelFullname)->getKeyName(), $tableName . '_' . $userTable . '_' . (new $modelFullname)->getKeyName())
                 ->references((new User)->getKeyName())
                 ->on($userTable)
                 ->onUpdate('cascade')
