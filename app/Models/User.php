6b8699a19bd054034ca4bd8a42a49764b34ddd0a
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-27 00:52:41 -0700

    Update \App\Models\User::authenticatableRole method

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Auth\User as Authenticatable;
 use App\Models\rules\DSCustom;
+use Database\Traits\ResolveUserModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Schema;
@@ -11,7 +12,8 @@ use Illuminate\Support\Facades\Schema;
 class User extends Authenticatable
 {
     use HasFactory,
-        Notifiable;
+        Notifiable,
+        ResolveUserModel;
 
     protected $table = "users";
 
@@ -19,12 +21,14 @@ class User extends Authenticatable
 
     public function authenticatableRole(): Authenticatable|null
     {
-        foreach ($allModelsFullnames = $this->getAllModelsFullname() as $modelFullname) {
-            if (array_search($this->getForeignKey(), Schema::getColumnListing((new $modelFullname)->getTable())) === false) {
+        foreach ($roles = Role::all() as $role) {
+            $modelFullname = $this->resolveRuleModelFullName($role->name);
+
+            if (!class_exists($modelFullname) || !is_subclass_of($modelFullname, Authenticatable::class)) {
                 continue;
             }
 
-            if (($authenticatable = $this->hasOne($modelFullname, $this->getForeignKey(), $this->getKeyName())->first()) === null) {
+            if (($authenticatable = $this->hasOne($modelFullname, (new $modelFullname)->getKeyName(), $this->getKeyName())->first()) === null) {
                 continue;
             }
 
