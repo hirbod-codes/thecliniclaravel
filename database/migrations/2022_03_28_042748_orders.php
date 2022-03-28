457f3ba042e3d2d05ee902012f30aa2d4ae9a83c
Hirbod <hirbod.khatami.words@gmail.com>
2022-03-28 05:14:10 -0700

    Create orders migration file

---
+++ database/migrations/2022_03_28_042748_orders.php
@@ -0,0 +1,53 @@
+<?php
+
+use App\Models\Order\Order;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private string $table;
+
+    public function __construct()
+    {
+        $this->table = (new Order)->getTable();
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (BluePrint $table) {
+            $table->id();
+
+            $fk = (new User)->getForeignKey();
+
+            $table->unsignedBigInteger($fk);
+            $table->foreign($fk, $this->table . '_' . (new User)->getTable() . '_' . $fk)
+            ->references((new User)->getKeyName())
+            ->on((new User)->getTable())
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists($this->table);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+};
