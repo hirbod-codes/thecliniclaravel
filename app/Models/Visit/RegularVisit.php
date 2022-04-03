6cc77e187e052b454733adf38576f6cdabf2e7e6
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 06:05:39 -0700

    Add methods for getting DSRegularVisits and DSRegularVisit in RegularVisit model

--- app/Models/Visit/RegularVisit.php
+++ app/Models/Visit/RegularVisit.php
@@ -42,4 +42,91 @@ class RegularVisit extends Model
             __FUNCTION__
         );
     }
+
+    public function getDSRegularVisit(): DSRegularVisit
+    {
+        $args = [];
+        array_map(function (\ReflectionParameter $parameter) use (&$args) {
+            $parameterName = $parameter->getName();
+
+            $this->collectDSArgs($args, $parameterName);
+        }, (new \ReflectionClass(DSRegularVisit::class))->getConstructor()->getParameters());
+
+        return new DSRegularVisit(...$args);
+    }
+
+    private function collectDSArgs(array &$args, string $parameterName)
+    {
+        if ($parameterName === 'id') {
+            $args[$parameterName] = $this->{$this->getKeyName()};
+        } elseif ($parameterName === 'weekDaysPeriods') {
+            if (($weekDaysPeriods = $this->week_days_periods) === null) {
+                $args[$parameterName] = null;
+            } else {
+                $args[$parameterName] = $this->getDSWeekDaysPeriods($weekDaysPeriods);
+            }
+        } elseif ($parameterName === 'dateTimePeriod') {
+            if (($dateTimePeriod = $this->date_time_period) === null) {
+                $args[$parameterName] = null;
+            } else {
+                $args[$parameterName] = $this->getDSDateTimePeriod($dateTimePeriod);
+            }
+        } else {
+            $args[$parameterName] = $this->{Str::snake($parameterName)};
+        }
+    }
+
+    /**
+     * @param \Iterator|self[]|Collection $regularVisits
+     * @return DSRegularVisits
+     */
+    public static function getDSRegularVisits(array|Collection $regularVisits, string $sort): DSRegularVisits
+    {
+
+        return self::getDSRegularVisitsConditionally($regularVisits, $sort, true);
+    }
+
+    /**
+     * @param \Iterator|self[]|Collection $regularVisits
+     * @return DSRegularVisits
+     */
+    public static function getMixedDSRegularVisits(array|Collection $regularVisits, string $sort): DSRegularVisits
+    {
+        return self::getDSRegularVisitsConditionally($regularVisits, $sort, false);
+    }
+
+    /**
+     * @param \Iterator|self[]|Collection $regularVisits
+     * @return DSRegularVisits
+     */
+    public static function getDSRegularVisitsConditionally(array|Collection $regularVisits, string $sort, bool $userSpecific): DSRegularVisits
+    {
+        $dsRegularVisits = new DSRegularVisits($sort);
+
+        if (count($regularVisits) === 0) {
+            return $dsRegularVisits;
+        }
+
+        $first = true;
+        /** @var self $regularVisit */
+        foreach ($regularVisits as $regularVisit) {
+            if (!($regularVisit instanceof RegularVisit)) {
+                throw new \InvalidArgumentException('The variable $regularVisit must be of type: ' . RegularVisit::class, 500);
+            }
+
+            if ($first && $userSpecific) {
+                $first = false;
+
+                $dsRegularVisits = new DSRegularVisits(
+                    $sort,
+                    $regularVisit->regularOrder->order->user->authenticatableRole()->getDataStructure(),
+                    $regularVisit->regularOrder->getDSRegularOrder()
+                );
+            }
+
+            $dsRegularVisits[] = $regularVisit->getDSRegularVisit();
+        }
+
+        return $dsRegularVisits;
+    }
 }
