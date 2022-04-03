bcf47d09a8562f9a7228ccde1c86b9314ed26935
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 06:20:01 -0700

    Add methods for getting DSOrders in Order model

--- app/Models/Order/Order.php
+++ app/Models/Order/Order.php
@@ -5,9 +5,11 @@ namespace App\Models\Order;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Model;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Str;
+use TheClinicDataStructures\DataStructures\Order\DSOrders;
 
 class Order extends Model
 {
@@ -74,9 +76,70 @@ class Order extends Model
             $this->getKeyName()
         );
     }
+
+    /**
+     * @param \Iterator|array|self[]|LaserOrder[]|RegularOrder[]|Collection $orders
+     * @return DSOrders
+     */
+    public static function getDSOrders(\Iterator|array|Collection $orders): DSOrders
+    {
+        return self::getDSOrdersConditionally($orders, true);
+    }
+
+    /**
+     * @param \Iterator|array|self[]|LaserOrder[]|RegularOrder[]|Collection $orders
+     * @return DSOrders
+     */
+    public static function getMixedDSOrders(\Iterator|array|Collection $orders): DSOrders
+    {
+        return self::getDSOrdersConditionally($orders, false);
+    }
+
+    /**
+     * @param \Iterator|array|self[]|LaserOrder[]|RegularOrder[]|Collection $orders
+     * @param boolean $userSpecific
+     * @return DSOrders
+     */
+    public static function getDSOrdersConditionally(\Iterator|array|Collection $orders, bool $userSpecific): DSOrders
+    {
+        $dsOrders = new DSOrders();
+        $first = true;
+        foreach ($orders as $order) {
+            if (!in_array(get_class($order), [Order::class, LaserOrder::class, RegularOrder::class])) {
+                throw new \InvalidArgumentException(
+                    'Only the following types are allowed: ' .
+                        Order::class . ' or ' .
+                        LaserOrder::class . ' or ' .
+                        RegularOrder::class . '.',
+                    500
+                );
             }
 
-            return $order;
+            if ($first && $userSpecific) {
+                $first = false;
+
+                if ($order instanceof Order) {
+                    $dsOrders = new DSOrders($order->user->authenticatableRole()->getDataStructure());
+                } elseif ($order instanceof RegularOrder) {
+                    $dsOrders = new DSOrders($order->order->user->authenticatableRole()->getDataStructure());
+                } elseif ($order instanceof LaserOrder) {
+                    $dsOrders = new DSOrders($order->order->user->authenticatableRole()->getDataStructure());
+                }
+            }
+
+            if ($order instanceof Order) {
+                if ($order->laserOrder !== null) {
+                    $dsOrders[] = $order->laserOrder->getDSLaserOrder();
+                } elseif ($order->regularOrder !== null) {
+                    $dsOrders[] = $order->regularOrder->getDSRegularOrder();
+                }
+            } elseif ($order instanceof RegularOrder) {
+                $dsOrders[] = $order->getDSRegularOrder();
+            } elseif ($order instanceof LaserOrder) {
+                $dsOrders[] = $order->getDSLaserOrder();
+            }
         }
+
+        return $dsOrders;
     }
 }
