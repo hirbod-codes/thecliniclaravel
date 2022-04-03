6e396d7439f5a491b33aa44850bcae2a25216cbb
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 05:48:29 -0700

    Create 'visits' table's migration file

---
+++ database/migrations/2022_03_29_060107_create_visits_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Visit\Visit;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private string $table;
+
+    public function __construct()
+    {
+        $this->table = (new Visit)->getTable();
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (BluePrint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists($this->table);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+};
