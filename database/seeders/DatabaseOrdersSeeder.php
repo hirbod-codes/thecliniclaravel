9bc7633019d750161becdff9b3a895cff5199a84
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 06:31:01 -0700

    Refactor DatabaseOrdersSeeder::run method

--- database/seeders/DatabaseOrdersSeeder.php
+++ database/seeders/DatabaseOrdersSeeder.php
@@ -8,7 +8,6 @@ use App\Models\Order\LaserOrderPart;
 use App\Models\Order\RegularOrder;
 use App\Models\Package\Package;
 use App\Models\Part\Part;
-use App\Models\roles\PatientRole;
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -29,55 +28,69 @@ class DatabaseOrdersSeeder extends Seeder
 
         /** @var \App\Models\User $user */
         foreach (User::get()->all() as $user) {
-            $order = $user->order()->create();
-            $orderId = $order->{$order->getKeyName()};
+            for ($i = 0; $i < $faker->numberBetween(2, 6); $i++) {
+                $order = $user->orders()->create();
+                $orderId = $order->{$order->getKeyName()};
 
-            if ($faker->randomElement(0, 1) === 0) {
-                $price = $faker->numberBetween(1000000, 20000000);
-                $time = $faker->numberBetween(600, 5400);
+                if ($i === 0) {
+                    regular:
+                    $price = $faker->numberBetween(1000000, 20000000);
+                    $time = $faker->numberBetween(600, 5400);
 
-                $regularOrder = RegularOrder::factory()
-                    ->setPrice($price)
-                    ->setNeedeTime($time)
-                    ->setOrderFK($orderId)
-                    ->create();
+                    $regularOrder = RegularOrder::factory()
+                        ->setPrice($price)
+                        ->setNeedeTime($time)
+                        ->setOrderFK($orderId)
+                        ->create();
 
-                continue;
-            }
+                    continue;
+                }
 
-            $gender = $faker->randomElement(['Male', 'Female']);
-            $parts = Part::query()->where('gender', '=', $gender)->inRandomOrder()->take($faker->numberBetween(1, 7))->get()->all();
-            $packages = Package::query()->where('gender', '=', $gender)->inRandomOrder()->take($faker->numberBetween(1, 3))->get()->all();
+                if ($i === 1) {
+                    laser:
+                    $gender = $user->gender;
+                    $parts = Part::query()->where('gender', '=', $gender)->inRandomOrder()->take($faker->numberBetween(1, 7))->get()->all();
+                    $packages = Package::query()->where('gender', '=', $gender)->inRandomOrder()->take($faker->numberBetween(1, 3))->get()->all();
 
-            $price = (new PriceCalculator)->calculateWithoutDiscount(Part::getDSParts($parts, $gender), Package::getDSPackages($packages, $gender));
-            $priceWithDiscount = (new PriceCalculator)->calculate(Part::getDSParts($parts, $gender), Package::getDSPackages($packages, $gender));
+                    $price = (new PriceCalculator)->calculateWithoutDiscount(Part::getDSParts($parts, $gender), Package::getDSPackages($packages, $gender));
+                    $priceWithDiscount = (new PriceCalculator)->calculate(Part::getDSParts($parts, $gender), Package::getDSPackages($packages, $gender));
 
-            $time = (new TimeConsumptionCalculator)->calculate(Part::getDSParts($parts, $gender), Package::getDSPackages($packages, $gender));
+                    $time = (new TimeConsumptionCalculator)->calculate(Part::getDSParts($parts, $gender), Package::getDSPackages($packages, $gender));
 
-            $laserOrder = LaserOrder::factory()
-                ->setPriceWithDiscount($priceWithDiscount)
-                ->setPrice($price)
-                ->setNeedeTime($time)
-                ->setOrderFK($orderId)
-                ->create();
-            $laserOrderId = $laserOrder->{$laserOrder->getKeyName()};
+                    $laserOrder = LaserOrder::factory()
+                        ->setPriceWithDiscount($priceWithDiscount)
+                        ->setPrice($price)
+                        ->setNeedeTime($time)
+                        ->setOrderFK($orderId)
+                        ->create();
+                    $laserOrderId = $laserOrder->{$laserOrder->getKeyName()};
 
-            foreach ($parts as $part) {
-                $partId = $part->{$part->getKeyName()};
+                    foreach ($parts as $part) {
+                        $partId = $part->{$part->getKeyName()};
 
-                LaserOrderPart::factory()
-                    ->setLaserOrderFK($laserOrderId)
-                    ->setPartFK($partId)
-                    ->create();
-            }
+                        LaserOrderPart::factory()
+                            ->setLaserOrderFK($laserOrderId)
+                            ->setPartFK($partId)
+                            ->create();
+                    }
 
-            foreach ($packages as $package) {
-                $packageId = $package->{$package->getKeyName()};
+                    foreach ($packages as $package) {
+                        $packageId = $package->{$package->getKeyName()};
 
-                LaserOrderPackage::factory()
-                    ->setLaserOrderFK($laserOrderId)
-                    ->setPackageFK($packageId)
-                    ->create();
+                        LaserOrderPackage::factory()
+                            ->setLaserOrderFK($laserOrderId)
+                            ->setPackageFK($packageId)
+                            ->create();
+                    }
+
+                    continue;
+                }
+
+                if ($faker->numberBetween(0, 1) === 0) {
+                    goto laser;
+                } else {
+                    goto regular;
+                }
             }
         }
     }
