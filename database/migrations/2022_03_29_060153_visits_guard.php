b6e0629e34123ab3af3813a9d6a6bbbf6f78ec8a
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 05:48:54 -0700

    Create 'visits_guard' table's migration file

---
+++ database/migrations/2022_03_29_060153_visits_guard.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private string $table;
+
+    public function __construct()
+    {
+        $this->table = 'visits_guard';
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (BluePrint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists($this->table);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+};
