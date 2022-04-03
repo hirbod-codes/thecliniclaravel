e860b85a2da6c53290ad17d02f0737a85ee95972
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-03 05:54:54 -0700

    Create 'business_defaults' table's migration file

---
+++ database/migrations/2022_03_29_110238_create_business_defaults_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\BusinessDefault;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private string $table;
+
+    public function __construct()
+    {
+        $this->table = (new BusinessDefault)->getTable();
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->id();
+
+            $table->integer('default_regular_order_price');
+            $table->integer('default_regular_order_time_consumption');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists($this->table);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+};
