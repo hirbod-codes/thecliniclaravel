4a4b417ac2fe617fceb456f824845dadf359f91f
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-04 22:48:39 -0700

    Update BusinessDefault model

--- app/Models/BusinessDefault.php
+++ app/Models/BusinessDefault.php
@@ -3,11 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use TheClinicDataStructures\DataStructures\Time\DSDownTimes;
+use TheClinicDataStructures\DataStructures\Time\DSWorkSchedule;
 
 class BusinessDefault extends Model
 {
     use HasFactory;
 
     protected $table = "business_defaults";
+
+    protected function workSchedule(): Attribute
+    {
+        return Attribute::make(
+            set: function (DSWorkSchedule $value) {
+                return json_encode($value->toArray());
+            }
+        );
+    }
+
+    protected function downTimes(): Attribute
+    {
+        return Attribute::make(
+            set: function (DSDownTimes $value) {
+                return json_encode($value->toArray());
+            }
+        );
+    }
 }
