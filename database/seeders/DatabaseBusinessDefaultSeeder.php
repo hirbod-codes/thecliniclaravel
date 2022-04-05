91d82f062f595b979cd8b262663a8d8c30b551d7
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-04 22:51:22 -0700

    Update DatabaseBusinessDefaultSeeder seeder class

--- database/seeders/DatabaseBusinessDefaultSeeder.php
+++ database/seeders/DatabaseBusinessDefaultSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\BusinessDefault;
+use Database\Factories\WorkScheduleFactory;
 use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use TheClinicDataStructures\DataStructures\Time\DSDownTimes;
 
 class DatabaseBusinessDefaultSeeder extends Seeder
 {
@@ -18,9 +20,14 @@ class DatabaseBusinessDefaultSeeder extends Seeder
     {
         $faker = Factory::create();
 
+        $dsWorkSchedule = (new WorkScheduleFactory())->generateWorkSchedule();
+        $dsDown_times = new DSDownTimes();
+
         $businessDefault = new BusinessDefault;
         $businessDefault->default_regular_order_price = $faker->numberBetween(5000000, 10000000);
         $businessDefault->default_regular_order_time_consumption = $faker->numberBetween(600, 1000);
+        $businessDefault->work_schedule = $dsWorkSchedule;
+        $businessDefault->down_times = $dsDown_times;
 
         if (!$businessDefault->save()) {
             throw new \Exception('Failed to create the business default record.');
