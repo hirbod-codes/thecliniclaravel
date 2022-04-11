38f832ada8bad315c06f776965456e7b5c87c3f6
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-11 06:28:16 -0700

    Create DataBaseCreateRole class

---
+++ database/interactions/Privileges/DataBaseCreateRole.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Interactions\Privileges;
+
+use App\Models\Privilege;
+use App\Models\PrivilegeValue;
+use App\Models\Role;
+use Illuminate\Support\Facades\DB;
+use TheClinicUseCases\Privileges\Interfaces\IDataBaseCreateRole;
+
+class DataBaseCreateRole implements IDataBaseCreateRole
+{
+    public function createRole(string $customRoleName, array $privilegeValue): void
+    {
+        DB::beginTransaction();
+        try {
+            $role = new Role();
+            $role->name = $customRoleName;
+            if (!$role->save()) {
+                throw new \RuntimeException('Failed to create the role model.', 500);
+            }
+
+            foreach ($privilegeValue as $privilege => $value) {
+                $privilegeValueModel = new PrivilegeValue;
+                $privilegeValueModel->privilegeValue = $value;
+                $privilegeValueModel->{(new Role)->getForeignKey()} = $role->getKey();
+                $privilegeValueModel->{(new Privilege)->getForeignKey()} = Privilege::query()
+                    ->where('name', '=', $privilege)
+                    ->first()->getKey();
+                if (!$privilegeValueModel->save()) {
+                    throw new \RuntimeException('Failed to create the privilege-value model.', 500);
+                }
+            }
+        } catch (\Throwable $th) {
+            DB::rollBack();
+        }
+    }
+}
