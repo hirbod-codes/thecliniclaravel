92953c96a958927c10b1e99727e7981569fe43ae
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-11 06:05:37 -0700

    Create CustomRole model

---
+++ app/Models/roles/CustomRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\roles;
+
+use App\Models\Model;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
+
+class CustomRole extends Model
+{
+    use HasFactory,
+        Notifiable;
+
+    protected $table = "custom_roles";
+
+    protected string $DS = DSCustom::class;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, $this->getKeyName(), (new User)->getKeyName(), __FUNCTION__);
+    }
+}
