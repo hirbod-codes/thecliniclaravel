36cde4b5c14ee21ac6c98ac4460ccfdcf4297642
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-11 06:11:49 -0700

    Update WorkScheduleFactory factory

--- database/factories/WorkScheduleFactory.php
+++ database/factories/WorkScheduleFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use TheClinicDataStructures\DataStructures\Time\DSDateTimePeriods;
 use TheClinicDataStructures\DataStructures\Time\DSDateTimePeriod;
+use TheClinicDataStructures\DataStructures\Time\DSWeekDaysPeriods;
 use TheClinicDataStructures\DataStructures\Time\DSWorkSchedule;
 
 /**
@@ -24,45 +25,37 @@ class WorkScheduleFactory extends Factory
 
     public function generateWorkSchedule(): DSWorkSchedule
     {
+        $time = new \DateTime;
         $dsWorkSchedule = new DSWorkSchedule('Monday');
 
         /** @var string $weekDay */
         foreach (DSWorkSchedule::$weekDays as $weekDay) {
-            $dsTimePeriods = new DSDateTimePeriods;
+            $this->moveToWeekDay($time, $weekDay);
+            $dsDateTimePeriods = new DSDateTimePeriods;
 
-            $previous = $this->findDateTimeInWeekDay($weekDay);
-            for ($i = 0; $i < 3; $i++) {
-                $dsTimePeriods[] = new DSDateTimePeriod(...($period = $this->makeDateTimePeriod($previous, '+5 minutes', '+5 hours')));
-                $previous = $period[1];
-            }
+            $dsDateTimePeriods[] = new DSDateTimePeriod(
+                (new \DateTime($time->format('Y-m-d')))->setTime(8, 0),
+                (new \DateTime($time->format('Y-m-d')))->setTime(15, 0)
+            );
 
-            $dsWorkSchedule[$weekDay] = $dsTimePeriods;
+            $dsDateTimePeriods[] = new DSDateTimePeriod(
+                (new \DateTime($time->format('Y-m-d')))->setTime(16, 0),
+                (new \DateTime($time->format('Y-m-d')))->setTime(23, 0)
+            );
+
+            $dsWorkSchedule[$weekDay] = $dsDateTimePeriods;
         }
         return $dsWorkSchedule;
     }
 
-    public function findDateTimeInWeekDay(string $weekDay): \DateTime
+    public function moveToWeekDay(\DateTime &$time, string $weekDay): void
     {
-        if (($now = (new \DateTime()))->format('l') === $weekDay) {
-            return $now;
+        if (!in_array($weekDay, DSWeekDaysPeriods::$weekDays)) {
+            throw new \InvalidArgumentException('Incorrect value for weekDay variable.');
         }
 
-        while ($now->format('l') !== $weekDay) {
-            $now->modify('+1 day');
+        while ($time->format('l') !== $weekDay) {
+            $time->modify('+1 day');
         }
-        return $now;
-    }
-
-    /**
-     * @param \DateTime $previous
-     * @param string $firstModify
-     * @param string $secondModify
-     * @return array [$startingDateTime, $endingDatTime]
-     */
-    public function makeDateTimePeriod(\DateTime &$previous, string $firstModify, string $secondModify): array
-    {
-        $s = (new \DateTime)->setTimestamp($previous->getTimestamp())->modify($firstModify);
-        $e = (new \DateTime)->setTimestamp($s->getTimestamp())->modify($firstModify);
-        return [$s, $e];
     }
 }
