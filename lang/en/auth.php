33e4f6095cb3262eab3cf16b25e3c056d9c60650
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-20 06:21:11 -0700

    Update ./lang/en/auth.php for phonenumber verification

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,6 +13,7 @@ return [
     |
     */
 
+    'phonenumber_verification_code_sent' => '{0} A 6-digit code has sent to your phonenumber.',
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
