ade93aa68f5511bb0031d6495a90162ac4256068
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-20 06:41:25 -0700

    Update App\Console\Kernel class

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('queue:prune-batches')->daily();
+        $schedule->command('queue:prune-batches --hours=168 --unfinished=72')->daily();
+        $schedule->command('queue:monitor database:default --max:700')->daily();
     }
 
     /**
@@ -25,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
