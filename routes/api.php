b839b8a90ea639052a6b66f8f8d954fca3325e34
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-24 00:14:16 -0700

    Update Routes

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\Orders\OrdersController;
 use App\Http\Controllers\RolesController;
 use App\Http\Controllers\Visits\VisitsController;
 use App\Http\Requests\UpdateLocaleRequest;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
@@ -22,104 +20,71 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('throttle:global')->group(function () {
-    Route::put('/updateLocale', function (UpdateLocaleRequest $request) {
-        $locale = $request->safe()->only('locale');
+Route::put('/updateLocale', function (UpdateLocaleRequest $request) {
+    $locale = $request->safe()->only('locale');
 
-        App::setLocale($locale);
+    App::setLocale($locale);
 
-        return response('The locale option successfully updated.', 200);
-    });
+    return response('The locale option successfully updated.', 200);
+});
 
-    Route::group(['middleware' => ['web', 'auth']], function ($router) {
-        $router->get('/clients', function () {
+Route::middleware('auth:api')->get('/logout', [AuthController::class, 'apiLogout'])->name('auth.apiLogout');
+Route::middleware('guest:api')->post('/login', [AuthController::class, 'apiLogin'])->name('auth.apiLogin');
+
+Route::middleware(['auth:api', 'start_session', 'phonenumber_not_verified'])->get('/register/verifyPhonenumber', fn () => view('auth.verify-phonenumber'))->name('auth.verifyPhonenumber.page');
+Route::middleware(['auth:api', 'start_session', 'phonenumber_not_verified'])->post('/register/verifyPhonenumber', [AuthController::class, 'verifyPhonenumber'])->name('auth.verifyPhonenumber');
+
+Route::middleware(['auth:api', 'phonenumber_verified'])->group(function () {
+    Route::controller(AccountsController::class)
+        ->group(function () {
+            Route::get('/accounts/{roleName?}/{count?}/{lastAccountId?}', 'index')->name('accounts.index');
+
+            // Route::post('/accounts/verifyPhonenumber', 'verifyPhonenumber')->name('accounts.verifyPhonenumber');
+            Route::post('/accounts', 'store')->name('accounts.store');
+
+            Route::get('/accounts/{username}', 'show')->name('accounts.show');
+            Route::get('/accounts', 'showSelf')->name('accounts.showSelf');
+
+            Route::put('/accounts/{accountId}', 'update')->name('accounts.update');
+            Route::put('/accounts', 'updateSelf')->name('accounts.updateSelf');
+
+            Route::delete('/accounts/{accountId}', 'destroy')->name('accounts.destroy');
+            Route::delete('/accounts', 'destroySelf')->name('accounts.destroySelf');
         });
 
-        $router->post('/clients', function () {
+    Route::controller(RolesController::class)
+        ->group(function () {
+            Route::get('/roles', 'index')->name('roles.index');
+
+            Route::get('/roles/{self?}/{accountId?}', 'show')->name('roles.show');
+
+            Route::put('/roles', 'update')->name('roles.update');
         });
 
-        $router->put('/clients/{client_id}', function () {
+    Route::controller(OrdersController::class)
+        ->group(function () {
+            Route::get('/orders/Laser/{priceOtherwiseTime?}/{username?}/{lastOrderId?}/{count?}/{operator?}/{price?}/{timeConsumption?}', 'laserIndex')->name('orders.laserIndex');
+            Route::get('/orders/Regular/{priceOtherwiseTime?}/{username?}/{lastOrderId?}/{count?}/{operator?}/{price?}/{timeConsumption?}', 'regularIndex')->name('orders.regularIndex');
+
+            Route::post('/orders', 'store')->name('orders.store');
+
+            Route::get('/orders/{businessName}/{accountId}/{orderId}', 'show')->name('orders.show');
+
+            Route::delete('/orders/{businessName}/{accountId}/{orderId}', 'destroy')->name('orders.destroy');
         });
 
-        $router->delete('/clients/{client_id}', function () {
+    Route::controller(VisitsController::class)
+        ->group(function () {
+            Route::get('/visits/Laser/{accountId}/{sortByTimestamp}/{laserOrderId?}/{timestamp?}', 'laserIndex')->name('visits.laserIndex');
+            Route::get('/visits/Regular/{accountId}/{sortByTimestamp}/{regularOrderId?}/{timestamp?}', 'regularIndex')->name('visits.regularIndex');
+
+            Route::post('/visits/regular', 'laserStore')->name('visits.laserStore');
+            Route::post('/visits/laser', 'regularStore')->name('visits.regularStore');
+
+            Route::get('/visits/Laser/{timestamp}', 'laserShow')->name('visits.laserShow');
+            Route::get('/visits/Regular/{timestamp}', 'regularShow')->name('visits.regularShow');
+
+            Route::delete('/visits/laser/{regularVisitId}/{targetUserId}', 'laserDestroy')->name('visits.laserDestroy');
+            Route::delete('/visits/regular/{regularVisitId}/{targetUserId}', 'laserDestroy')->name('visits.laserDestroy');
         });
-    });
-
-    $authMiddleware = 'auth:' . implode(', ', array_keys(app()['config']['auth.guards']));
-
-    // Email verification
-    Route::get('/email/verify', function () {
-        return view('auth.verify-email');
-    })->middleware($authMiddleware)->name('verification.notice');
-
-    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-        $request->fulfill();
-
-        return response('', 200);
-    })->middleware([$authMiddleware, 'signed'])->name('verification.verify');
-
-    Route::post('/email/verification-notification', function (Request $request) {
-        $request->user()->sendEmailVerificationNotification();
-
-        return response('Verification link sent!', 200);
-    })->middleware([$authMiddleware, 'throttle:1,1'])->name('verification.send');
-
-    Route::middleware(['guest', 'throttle:6,1'])->post('/register/verifyPhonenumber', [AccountsController::class, 'verifyPhonenumber'])->name('auth.verifyPhonenumber');
-    Route::middleware('guest')->post('/register', [AuthController::class, 'register'])->name('auth.register');
-    Route::middleware($authMiddleware)->get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-
-    Route::middleware([$authMiddleware, 'throttle:api'])->group(function () use ($authMiddleware) {
-        Route::controller(AccountsController::class)
-            ->group(function () {
-                Route::get('/accounts/{roleName?}/{count?}/{lastAccountId?}', 'index')->name('accounts.index');
-
-                Route::post('/accounts/verifyPhonenumber', 'verifyPhonenumber')->middleware('throttle:6,1')->name('accounts.verifyPhonenumber');
-                Route::post('/accounts', 'store')->name('accounts.store');
-
-                Route::get('/accounts/{username}', 'show')->name('accounts.show');
-                Route::get('/accounts', 'showSelf')->name('accounts.showSelf');
-
-                Route::put('/accounts/{accountId}', 'update')->name('accounts.update');
-                Route::put('/accounts', 'updateSelf')->name('accounts.updateSelf');
-
-                Route::delete('/accounts/{accountId}', 'destroy')->name('accounts.destroy');
-                Route::delete('/accounts', 'destroySelf')->name('accounts.destroySelf');
-            });
-
-        Route::controller(RolesController::class)
-            ->group(function () {
-                Route::get('/roles', 'index')->name('roles.index');
-
-                Route::get('/roles/{self?}/{accountId?}', 'show')->name('roles.show');
-
-                Route::put('/roles', 'update')->name('roles.update');
-            });
-
-        Route::controller(OrdersController::class)
-            ->group(function () {
-                Route::get('/orders/Laser/{priceOtherwiseTime?}/{username?}/{lastOrderId?}/{count?}/{operator?}/{price?}/{timeConsumption?}', 'laserIndex')->name('orders.laserIndex');
-                Route::get('/orders/Regular/{priceOtherwiseTime?}/{username?}/{lastOrderId?}/{count?}/{operator?}/{price?}/{timeConsumption?}', 'regularIndex')->name('orders.regularIndex');
-
-                Route::post('/orders', 'store')->name('orders.store');
-
-                Route::get('/orders/{businessName}/{accountId}/{orderId}', 'show')->name('orders.show');
-
-                Route::delete('/orders/{businessName}/{accountId}/{orderId}', 'destroy')->name('orders.destroy');
-            });
-
-        Route::controller(VisitsController::class)
-            ->group(function () {
-                Route::get('/visits/Laser/{accountId}/{sortByTimestamp}/{laserOrderId?}/{timestamp?}', 'laserIndex')->name('visits.laserIndex');
-                Route::get('/visits/Regular/{accountId}/{sortByTimestamp}/{regularOrderId?}/{timestamp?}', 'regularIndex')->name('visits.regularIndex');
-
-                Route::post('/visits/regular', 'laserStore')->name('visits.laserStore');
-                Route::post('/visits/laser', 'regularStore')->name('visits.regularStore');
-
-                Route::get('/visits/Laser/{timestamp}', 'laserShow')->name('visits.laserShow');
-                Route::get('/visits/Regular/{timestamp}', 'regularShow')->name('visits.regularShow');
-
-                Route::delete('/visits/laser/{regularVisitId}/{targetUserId}', 'laserDestroy')->name('visits.laserDestroy');
-                Route::delete('/visits/regular/{regularVisitId}/{targetUserId}', 'laserDestroy')->name('visits.laserDestroy');
-            });
-    });
 });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +17,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('auth.forgot-password');
     return 200;
 })->name('home');
+
+Route::middleware('auth:web')->group(function () {
+    // Verify Email
+    Route::get('/email/verify', function () {
+        return view('auth.verify-email');
+    })->name('verification.notice');
+
+    Route::post('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Verification link sent!');
+    })->name('verification.send');
+
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect('/home');
+    })->middleware('signed')->name('verification.verify');
+
+    // Reset Password
+    Route::get('/forgot-password', fn () => view('auth.forgot-password'))->name('forgot_password.page');
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot_password');
+
+    Route::get('/reset-password', fn () => view('auth.reset-password'))->name('reset_password.pqge');
+    Route::put('/reset-password', [AuthController::class, 'resetPassword'])->name('reset_password');
+
+    // Logout
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
+
+// Register
+Route::middleware('guest:web')->get('/register', fn () => view('auth.register'))->name('auth.register.page');
+Route::middleware('guest:web')->post('/register', [AuthController::class, 'register'])->name('auth.register');
+
+// Login
+Route::middleware('guest:web')->get('/login', fn () => view('auth.login'))->name('auth.login.page');
+Route::middleware('guest:web')->post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+// Verify Phonenumber
+Route::middleware('phonenumber_not_verified')->get('/register/verifyPhonenumber', fn () => view('auth.verify-phonenumber'))->name('auth.verifyPhonenumber.page');
+Route::middleware('phonenumber_not_verified')->post('/register/verifyPhonenumber', [AuthController::class, 'verifyPhonenumber'])->name('auth.verifyPhonenumber');
