2b89e34bd593d8fa26148700cf464317b2ef7b87
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-26 23:52:29 -0700

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,23 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="/css/forgot-password.css">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="/css/base.css">
 </head>
 
 <body>
     <div class="parent_box" id="parent_box">
         <div class="box" id="box">
-            <div class="message">
-                Please enter your Email or Phonenumber:
-            </div>
+            <div class="message">Please enter your Email or Phonenumber:</div>
 
-            <input class="input" type="email" name="email" id="email" oninput="emailInput(this.value)"
-                title="email" placeholder="email@example.com" value="">
-
-            <input class="input" type="text" name="phonenumber" id="phonenumber"
-                oninput="phonenumberInput(this.value)" title="phonenumber" placeholder="09#########" value="">
+            <input class="input" type="email" name="email" id="email" oninput="emailInput(this.value)" title="email" placeholder="email@example.com" value="">
+            <input class="input" type="text" name="phonenumber" id="phonenumber" oninput="phonenumberInput(this.value)" title="phonenumber" placeholder="09#########" value="">
 
             <button class="button" id="button" type="button" onclick="send()">Send</button>
         </div>
