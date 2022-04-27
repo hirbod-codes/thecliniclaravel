799c9617dcf0f5f5a03eef07599158cd8ea35379
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-26 23:52:12 -0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,25 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/forgot-password.css">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="/css/base.css">
 </head>
 
 <body>
     <div class="parent_box" id="parent_box">
         <div class="box" id="box">
-            <div class="message">
-                Please enter your Email or Username:
-            </div>
+            <div class="message"> Please enter your Email or Username: </div>
 
-            <input class="input" type="email" name="email" id="email" oninput="emailInput(this.value)"
-                title="email" placeholder="email@example.com" value="">
-
-            <input class="input" type="text" name="username" id="username" oninput="usernameInput(this.value)"
-                title="username" placeholder="Username" value="">
-
-            <input class="input" type="password" name="password" id="password" title="password"
-                placeholder="Password" value="">
+            <input class="input" type="email" name="email" id="email" oninput="emailInput(this.value)" title="email" placeholder="email@example.com" value="">
+            <input class="input" type="text" name="username" id="username" oninput="usernameInput(this.value)" title="username" placeholder="Username" value="">
+            <input class="input" type="password" name="password" id="password" title="password" placeholder="Password" value="">
 
             <button class="button" id="button" type="button" onclick="submit()">Submit</button>
 
