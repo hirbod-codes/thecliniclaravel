eb20aac94850dbef945482bd132b5df0d64d6baf
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-28 04:55:03 -0700

    Create PresenceProhibitedWithout rule class

---
+++ app/Rules/PresenceProhibitedWithout.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\Rule;
+
+class PresenceProhibitedWithout implements Rule, DataAwareRule
+{
+    /**
+     * All of the data under validation.
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * @var string[]
+     */
+    private array $fields;
+
+    private string $duplicatedField;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct(array $fields)
+    {
+        $this->fields = $fields;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $this->attribute = $attribute;
+        foreach ($this->fields as $field) {
+            if (array_search($field, array_keys($this->data)) !== false) {
+                $this->duplicatedField = $field;
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans_choice('validation.presence_prohibited_with', 0, ['current_field' => $this->attribute, 'other_field' => $this->duplicatedField]);
+    }
+
+    /**
+     * Set the data under validation.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
