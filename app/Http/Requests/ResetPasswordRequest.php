f763d036354f89c9ade300a6ac4bf1c1177f4261
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-28 04:43:38 -0700

    Create ResetPasswordRequest request class

---
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\ProhibitExtraFeilds;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = (new ForgotPasswordRequest())->initialRules();
+
+        $rules['code'] = ['required', 'integer', 'numeric'];
+        $rules['password'] = (include(base_path() . '/app/Rules/BuiltInRules/Models/User/password.php'))['password'];
+
+        $rules['email'][] = new ProhibitExtraFeilds($rules);
+
+        return $rules;
+    }
+}
