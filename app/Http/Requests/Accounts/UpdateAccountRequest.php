73cf8082675835a47e5fc9504a11f89f68dd6baf
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-28 04:40:24 -0700

    Update UpdateAccountRequest request class

--- app/Http/Requests/Accounts/UpdateAccountRequest.php
+++ app/Http/Requests/Accounts/UpdateAccountRequest.php
@@ -26,13 +26,13 @@ class UpdateAccountRequest extends FormRequest
     public function rules()
     {
         $array = [
-            'phonenumber' => include (base_path() . '/app/Rules/BuiltInRules/Models/User/phonenumber.php')['phonenumber_optional'],
-            'firstname' => include (base_path() . '/app/Rules/BuiltInRules/Models/User/firstname.php')['firstname_optional'],
-            'lastname' => include (base_path() . '/app/Rules/BuiltInRules/Models/User/lastname.php')['lastname_optional'],
-            'username' => include (base_path() . '/app/Rules/BuiltInRules/Models/User/username.php')['username_optional'],
-            'email' => include (base_path() . '/app/Rules/BuiltInRules/Models/User/email.php')['email_optional'],
-            'gender' => include (base_path() . '/app/Rules/BuiltInRules/Models/User/gender.php')['gender_optional'],
-            'avatar' => include (base_path() . '/app/Rules/BuiltInRules/Models/avatar.php')['avatar_optional'],
+            'phonenumber' => (include(base_path() . '/app/Rules/BuiltInRules/Models/User/phonenumber.php'))['phonenumber_optional'],
+            'firstname' => (include(base_path() . '/app/Rules/BuiltInRules/Models/User/firstname.php'))['firstname_optional'],
+            'lastname' => (include(base_path() . '/app/Rules/BuiltInRules/Models/User/lastname.php'))['lastname_optional'],
+            'username' => (include(base_path() . '/app/Rules/BuiltInRules/Models/User/username.php'))['username_optional'],
+            'email' => (include(base_path() . '/app/Rules/BuiltInRules/Models/User/email.php'))['email_optional'],
+            'gender' => (include(base_path() . '/app/Rules/BuiltInRules/Models/User/gender.php'))['gender_optional'],
+            'avatar' => (include(base_path() . '/app/Rules/BuiltInRules/Models/avatar.php'))['avatar_optional'],
         ];
 
         $array['username'][] = new ProhibitExtraFeilds($array);
