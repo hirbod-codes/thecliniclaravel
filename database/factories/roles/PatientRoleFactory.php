590de4d46212f53e04acd4d6fb92098654955bea
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-30 00:29:31 -0700

    Update PatientRoleFactory factory class

--- database/factories/roles/PatientRoleFactory.php
+++ database/factories/roles/PatientRoleFactory.php
@@ -3,13 +3,20 @@
 namespace Database\Factories\roles;
 
 use App\Models\roles\PatientRole;
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PatientRoleFactory extends Factory
 {
     public function definition()
     {
-        return [];
+        return [
+            'age' => $this->faker->numberBetween(16, 70),
+            'state' => $this->faker->state(),
+            'city' => $this->faker->city(),
+            'address' => $this->faker->address(),
+            'laser_grade' => $this->faker->numerify() . '/' . $this->faker->numerify(),
+        ];
     }
 
     public function usersForeignKey(int $value): static
