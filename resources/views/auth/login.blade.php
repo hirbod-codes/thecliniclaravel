84d87807540f8ca43898b81a028067eb16c62dcf
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-30 07:31:39 -0700

    Update login blade view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }} | {{ trans_choice('auth.Login', 0) }}</title>
     <link rel="stylesheet" href="/css/base.css">
 </head>
 
