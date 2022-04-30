9cbe15438eee2351f2825655dd3e95c875030ee9
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-30 07:33:31 -0700

    Update DataBaseDeleteAccount class

--- database/interactions/Accounts/DataBaseDeleteAccount.php
+++ database/interactions/Accounts/DataBaseDeleteAccount.php
@@ -2,8 +2,11 @@
 
 namespace Database\Interactions\Accounts;
 
+use App\Models\User;
 use Database\Traits\ResolveUserModel;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use TheClinicDataStructures\DataStructures\User\DSUser;
 use TheClinicUseCases\Accounts\Interfaces\IDataBaseDeleteAccount;
 
@@ -13,17 +16,29 @@ class DataBaseDeleteAccount implements IDataBaseDeleteAccount
 
     public function deleteAccount(DSUser $user): void
     {
-        $theModelClassFullName = $this->resolveRuleModelFullName($this->resolveRuleName($user));
+        try {
+            DB::beginTransaction();
 
-        $theModel = $theModelClassFullName::where((new $theModelClassFullName)->getKeyName(), $user->getId())->first();
+            $theModelClassFullName = $this->resolveRuleModelFullName($this->resolveRuleName($user));
 
-        if ($theModel === null) {
-            throw new ModelNotFoundException('Failed to find the user.', 404);
+            $theModel = $theModelClassFullName::where((new $theModelClassFullName)->getKeyName(), $user->getId())->first();
+
+            if ($theModel === null) {
+                throw new ModelNotFoundException('Failed to find the user.', 404);
+            }
+
+            /** @var User $theUserModel */
+            $theUserModel = $theModel->user;
+
+            $theModel->delete();
+            $theUserModel->delete();
+
+            DB::commit();
+
+            Storage::disk('local')->delete('images/avatars' . strval($theUserModel->getKey()));
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
         }
-
-        $theUserModel = $theModel->user()->first();
-
-        $theModel->delete();
-        $theUserModel->delete();
     }
 }
