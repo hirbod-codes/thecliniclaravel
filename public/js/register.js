ab9ce7d4fda4a55c39d42c2d8a5075d77e85c03a
Hirbod <hirbod.khatami.words@gmail.com>
2022-04-30 00:38:14 -0700

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,29 +1,31 @@
+addStates(getStates());
+addGenders(getGenders());
+
 function register() {
     if (!isRequiredInputsFilled()) {
         return;
     }
 
-    var data = {
-        'firstname': getElmValue('firstname'),
-        'lastname': getElmValue('lastname'),
-        'username': getElmValue('username'),
-        'phonenumber': getElmValue('phonenumber'),
-        'password': getElmValue('password'),
-        'password_confirmation': getElmValue('password_confirmation'),
-        'age': getElmValue('age'),
-        'gender': getElmValue('gender'),
-        'state': getElmValue('state'),
-        'city': getElmValue('city'),
-        'address': getElmValue('address'),
-        'avatar': getElmValue('avatar'),
-    };
+    const data = new FormData();
+    data.append('firstname', getElmValue('firstname'));
+    data.append('lastname', getElmValue('lastname'));
+    data.append('username', getElmValue('username'));
+    data.append('phonenumber', getElmValue('phonenumber'));
+    data.append('password', getElmValue('password'));
+    data.append('password_confirmation', getElmValue('password_confirmation'));
+    data.append('age', getElmValue('age'));
+    data.append('gender', getElmValue('gender'));
+    data.append('state', getElmValue('state'));
+    data.append('city', getElmValue('city'));
+    data.append('address', getElmValue('address'));
+    data.append('avatar', getElm('avatar').files[0]);
 
     if (getElmValue('email')) {
-        data['email'] = getElmValue('email');
+        data.append('email', getElmValue('email'));
     }
 
     if (getElmValue('address')) {
-        data['address'] = getElmValue('address');
+        data.append('address', getElmValue('address'));
     }
 
     postData('/register', data)
@@ -75,22 +77,24 @@ function isRequiredInputsFilled() {
         if (Object.hasOwnProperty.call(elms, elm)) {
             const element = elms[elm];
 
-            if (element.nodeName !== 'INPUT' || element.nodeName !== 'TEXTAREA') {
+            if (!element.hasAttribute('required')) {
                 continue;
             }
 
-            if (element.hasAttribute('required') === true && isElmEmpty(element.id)) {
+            if (isElmEmpty(element.id)) {
                 if (first === true) {
                     first = false;
                     getElm('box').scroll(0, element.offsetTop - element.offsetHeight - 5);
                 }
-                element.setAttribute('style', 'border: 1px solid red;');
+                element.style.border = '1px solid red';
                 filled = false;
             }
         }
     }
 
     if (getElmValue('password') !== getElmValue('password_confirmation')) {
+        getElmValue('password').style.border = '1px solid red';
+        getElmValue('password_confirmation').style.border = '1px solid red';
         filled = false;
     }
 
@@ -98,19 +102,19 @@ function isRequiredInputsFilled() {
 }
 
 function firstname(val) {
-    getElm('firstname').removeAttribute('style');
+    getElm('firstname').style.border = 0;
 }
 
 function lastname(val) {
-    getElm('lastname').removeAttribute('style');
+    getElm('lastname').style.border = 0;
 }
 
 function username(val) {
-    getElm('username').removeAttribute('style');
+    getElm('username').style.border = 0;
 }
 
 function phonenumber(val) {
-    getElm('phonenumber').removeAttribute('style');
+    getElm('phonenumber').style.border = 0;
 
     if (val.match(/^(09){1}[0-9]{9}$/) === null) {
         getElm('phonenumber').setAttribute('style', 'border: 1px solid red');
@@ -118,7 +122,7 @@ function phonenumber(val) {
 }
 
 function phonenumberVerification(val) {
-    getElm('phonenumber_verification').removeAttribute('style');
+    getElm('phonenumber_verification').style.border = 0;
 
     if (val.match(/^(09){1}[0-9]{9}$/) === null) {
         getElm('phonenumber_verification').setAttribute('style', 'border: 1px solid red');
@@ -126,29 +130,115 @@ function phonenumberVerification(val) {
 }
 
 function password(val) {
-    getElm('password').removeAttribute('style');
+    getElm('password').style.border = 0;
 }
 
 function password_confirmation(val) {
-    getElm('password_confirmation').removeAttribute('style');
+    getElm('password_confirmation').style.border = 0;
+}
+
+function getGenders() {
+    return getData('/api/genders').then((response) => {
+        return getBody(response);
+    });
+}
+
+function addGenders(gendersPromise) {
+    gendersPromise.then((data) => {
+        data.forEach(gender => {
+            createAppendElm('option', {
+                'value': gender
+            }, getElm('gender')).innerText = gender;
+        });
+    });
 }
 
 function gender(val) {
-    getElm('gender').removeAttribute('style');
+    if (getElm('gender-label')) {
+        getElm('gender-label').remove();
+    }
+
+    getElm('gender').style.border = 0;
 }
 
 function age(val) {
-    getElm('age').removeAttribute('style');
-}
-
-function state(val) {
-    getElm('state').removeAttribute('style');
-}
-
-function city(val) {
-    getElm('city').removeAttribute('style');
+    getElm('age').style.border = 0;
 }
 
 function avatar(val) {
     getElm('avatar-label').children[0].innerText = 'Select Image: ' + getElm('avatar').files[0].name;
+    Array.from(getElm('img').children).forEach((elm) => {
+        elm.remove();
+    });
+    createAppendElm('img', {
+        'src': URL.createObjectURL(getElm('avatar').files[0]),
+        'style': 'object-fit:contain;width:100%;max-height:100%;'
+    }, getElm('img'));
+}
+
+function getStates() {
+    return getData('/api/states')
+        .then((response) => {
+            return getBody(response);
+        });
+}
+
+function addStates(statesPromise) {
+    statesPromise.then((states) => {
+        for (let i = 0; i < states.length; i++) {
+            const state = states[i];
+
+            createAppendElm('option', {
+                'value': state
+            }, getElm('state')).innerText = state;
+        }
+    });
+}
+
+function getCities(val) {
+    return getData('/api/cities?name=' + val).then((response) => {
+        return getBody(response);
+    });
+}
+
+function removeCities() {
+    var city = document.getElementById('city');
+    Array.from(city.children).forEach((elm) => { elm.remove(); });
+}
+
+function addCities(citiesPromise) {
+    citiesPromise.then((cities) => {
+        for (let i = 0; i < cities.length; i++) {
+            const city = cities[i];
+
+            createAppendElm('option', {
+                'id': city,
+                'value': city
+            }, getElm('city')).innerText = city;
+        }
+    });
+}
+
+function enableCity() {
+    getElm('city').removeAttribute('disabled');
+}
+
+function state(val) {
+    if (getElm('state-label')) {
+        getElm('state-label').remove();
+    }
+
+    getElm('state').style.border = 0;
+
+    removeCities();
+    addCities(getCities(val));
+    enableCity();
+}
+
+function city(val) {
+    if (getElm('city-label')) {
+        getElm('city-label').remove();
+    }
+
+    getElm('city').style.border = 0;
 }
