ab38df380fb83f616f161f76dc2de889e95deb1e
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-02 21:04:54 -0700

    Create SetAvatarRequest request class

---
+++ app/Http/Requests/AccountDocuments/SetAvatarRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\AccountDocuments;
+
+use App\Rules\ProhibitExtraFeilds;
+use Illuminate\Foundation\Http\FormRequest;
+
+class SetAvatarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $array = [
+            'accountId' => ['required', 'integer', 'numeric', 'min:1'],
+            'avatar' => (include(base_path() . '/app/Rules/BuiltInRules/Models/avatar.php'))['avatar'],
+        ];
+
+        $array['accountId'][] = new ProhibitExtraFeilds($array);
+
+        return $array;
+    }
+}
