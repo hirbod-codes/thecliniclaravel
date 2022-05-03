fdb7351cc46eaacb658d8cd84a26453558a0e2eb
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-02 21:03:04 -0700

    Update ./lang/en/Visits/visits.php

--- lang/en/Visits/visits.php
+++ lang/en/Visits/visits.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'visit_alert' => "{0} Please prepare for your visit due at :visit_date ."
+    'visit_alert' => "{0} Please prepare for your visit due at :visit_date .",
+    'visit-not-found' => "{0} There is no visit with such provided information.",
+    'destroy' => "{0} The visit successfully deleted.",
 ];
