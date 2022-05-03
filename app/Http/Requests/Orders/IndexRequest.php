45cae8f562a445e9476b48fbadd8d3adccaef7df
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-02 20:48:40 -0700

    Create IndexRequest request class

---
+++ app/Http/Requests/Orders/IndexRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Orders;
+
+use App\Rules\ProhibitExtraFeilds;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $array = [
+            'username' =>           ['required_without:count', 'prohibits:count,lastOrderId', 'string'],
+            'count' =>              ['required_without:username', 'string', 'prohibits:username'],
+            'lastOrderId' =>        ['string', 'numeric', 'integer'],
+            'priceOtherwiseTime' => ['boolean'],
+            'operator' =>           ['string', 'prohibited_if:priceOtherwiseTime,null', 'required_with:priceOtherwiseTime', Rule::in(['<', '>', '=', '<>', '>=', '<='])],
+            'price' =>              ['numeric', 'integer', 'prohibited_if:priceOtherwiseTime,null', 'prohibits:timeConsumption', 'required_if:priceOtherwiseTime,true'],
+            'timeConsumption' =>    ['numeric', 'integer', 'prohibited_if:priceOtherwiseTime,null', 'prohibits:price', 'required_if:priceOtherwiseTime,false'],
+        ];
+
+        $array['username'][] = new ProhibitExtraFeilds($array);
+        $array['count'][] = new ProhibitExtraFeilds($array);
+
+        return $array;
+    }
+}
