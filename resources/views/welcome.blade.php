efd213eca175576af4c388a2f701b3c01b903457
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-09 12:48:01 -0700

    Create welcome.blade.php

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/backend/css/base.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="parent_box" id="parent_box">
+        <div class="box" id="box">
+        </div>
+    </div>
+</body>
+
+<script src="/backend/js/base.js"></script>
+<script src="/backend/js/welcome.js"></script>
+
+</html>
