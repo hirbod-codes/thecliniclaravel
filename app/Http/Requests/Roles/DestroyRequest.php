f0ea6c06178c3a69e195a20f1969e30abe130a19
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-14 04:18:57 -0700

    Create DestroyRequest request class

---
+++ app/Http/Requests/Roles/DestroyRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Roles;
+
+use App\Models\Role;
+use App\Rules\ProhibitExtraFeilds;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use TheClinicDataStructures\DataStructures\User\DSUser;
+
+class DestroyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $array = [
+            'customRoleName' => ['required', 'string', Rule::notIn(array_merge(DSUser::$roles, ['custom'])), 'exists:' . (new Role)->getTable() . ',name'],
+        ];
+
+        $array[array_key_first($array)][] = new ProhibitExtraFeilds($array);
+
+        return $array;
+    }
+}
