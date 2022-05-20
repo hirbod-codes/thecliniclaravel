b9d2a9c26ea09f4fffec54f4cc1626e72993ac2a
Hirbod <hirbod.khatami.words@gmail.com>
2022-05-20 02:13:54 -0700

    Create ShowRequest Privilege request class

---
+++ app/Http/Requests/Privileges/ShowRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Privileges;
+
+use App\Rules\ProhibitExtraFeilds;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShowRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $array = [
+            'roleName' => (include(base_path() . '/app/Rules/BuiltInRules/Models/role.php'))['roleName'],
+        ];
+
+        $array[array_key_first($array)][] = new ProhibitExtraFeilds($array);
+
+        return $array;
+    }
+}
