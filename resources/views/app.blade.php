12cf8cc8e0bd2ad1a295bffa00ec2ed1ce7889b7
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-04 21:49:34 -0700

    Create app blade view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,18 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/backend/css/base.css">
-    <title>Document</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <title>{{ $title ?? env('APP_NAME', 'Document') }}</title>
 </head>
 
 <body>
-    <div class="parent_box" id="parent_box">
-        <div class="box" id="box">
-        </div>
-    </div>
+    <div id="root"></div>
 </body>
-
-<script src="/backend/js/base.js"></script>
-<script src="/backend/js/welcome.js"></script>
 
 </html>
