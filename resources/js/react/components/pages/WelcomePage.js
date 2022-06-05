a28fd68be59fe8ebb426c0b6143644733ddfdfde
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-04 21:51:59 -0700

    Create WelcomePage.js

---
+++ resources/js/react/components/pages/WelcomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class WelcomePage extends Component {
+    render() {
+        return (
+            <div>WelcomePage</div>
+        )
+    }
+}
+
+export default WelcomePage
