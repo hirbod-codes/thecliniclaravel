4a4d0d499846a7706275e51323d991a2d0fa4efd
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-09 01:13:31 -0700

    Update LogInPage react component

--- resources/js/react/components/pages/auth/LogInPage.js
+++ resources/js/react/components/pages/auth/LogInPage.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
-import LogInHeader from '../../headers/LogInHeader.js';
 import LogInForm from '../../../forms/auth/LogInForm.js';
+import Header from '../../headers/Header.js';
 
+import Button from '@mui/material/Button';
 import { Grid } from '@mui/material';
+import { translate } from '../../../traslation/translate.js';
 
 export class LogInPage extends Component {
     render() {
@@ -11,14 +14,21 @@ export class LogInPage extends Component {
             <>
                 <Grid container spacing={1} >
                     <Grid item xs={12} >
-                        <LogInHeader />
+                        <Header
+                            title={<Link to='/' style={{ textDecoration: 'none', color: 'white' }} >{translate('general/log-in/ucFirstLetterAllWords', this.props.currentLocaleName)}</ Link>}
+                            rightSide={
+                                <Button type='button' sx={{ a: { textDecoration: 'none', color: 'white' }, m: 1 }}>
+                                    <Link to="/register" >{translate('general/sign-up/ucFirstLetterAllWords', this.props.currentLocaleName)}</Link>
+                                </Button>
+                            }
+                        />
                     </Grid>
                     <Grid item xs={12} >
                         <Grid container >
                             <Grid item xs >
                             </Grid>
                             <Grid item xs={12} sm={9} md={6} >
-                                <LogInForm />
+                                <LogInForm currentLocaleName={this.props.currentLocaleName} />
                             </Grid>
                             <Grid item xs >
                             </Grid>
