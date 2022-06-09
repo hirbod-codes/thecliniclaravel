ae15d07ecf8e43583cf15ba1c0c484fa3d8b8ee8
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-09 01:17:13 -0700

    Update App react component

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -19,43 +19,117 @@ import { ForgotPassword } from "./components/pages/auth/ForgotPassword.js";
 
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
-import { Box } from '@mui/system';
 import { GlobalStyles } from '@mui/material';
+import { backendURL, getJsonData, postJsonData, putJsonData } from './components/Http/fetch';
 
 export class App extends Component {
     constructor(props) {
         super(props);
 
-        this.changeTheme = (name) => {
-            this.setState((state) => {
-                state.themeContext.theme = createTheme(resolveTheme(name));
-                state.themeContext.currentTheme = name.slice(0, name.indexOf('-'));
-                return state;
-            });
+        this.changeLocale = (name) => {
+            console.log(name);
+            putJsonData(backendURL() + '/locale', { 'locale': name }, { 'X-CSRF-TOKEN': this.state.token, 'Content-type': '*/*' })
+                .then((res) => {
+                    if (res.status === 200) {
+                        document.location.reload();
+                    }
+
+                    return res.text();
+                });
         };
 
-        this.changeLocale = (name) => {
-            this.setState((state) => {
-                state.localeContext.currentLocale = name;
-                return state;
-            });
+        this.changeTheme = (name) => {
+            console.log(name);
+            postJsonData(backendURL() + '/theme', { theme: name }, { 'X-CSRF-TOKEN': this.state.token })
+                .then((res) => {
+                    if (res.status === 200) {
+                        document.location.reload();
+                    }
+
+                    return res.json();
+                });
         };
 
         this.state = {
+            token: document.head.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+            isComponentLoading: true,
             themeContext: {
-                theme: createTheme(resolveTheme('light-ltr')),
+                theme: {},
                 changeTheme: this.changeTheme,
-                currentTheme: 'light'
+                currentTheme: '',
+                isThemeLoading: true,
             },
             localeContext: {
-                currentLocale: 'en',
+                locales: {},
+                currentLocale: null,
+                isLocaleLoading: true,
                 changeLocale: this.changeLocale
             }
         };
     }
 
+    shouldComponentUpdate() {
+        return !this.state.isComponentLoading;
+    }
+
+    componentDidMount() {
+        let state = this.state;
+
+        this.getLocales()
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                state.localeContext.locales = data;
+
+                this.getCurrentLocale()
+                    .then((res) => {
+                        return res.json();
+                    })
+                    .then((data) => {
+                        state.localeContext.currentLocale = data;
+                        state.localeContext.isLocaleLoading = false;
+
+                        this.getCurrentTheme()
+                            .then((res) => {
+                                return res.json();
+                            })
+                            .then((data) => {
+                                state.themeContext.theme = createTheme(resolveTheme(data.theme + '-' + state.localeContext.currentLocale.direction));
+                                state.themeContext.currentTheme = data.theme;
+                                state.themeContext.isThemeLoading = false;
+
+                                state.isComponentLoading = false;
+
+                                document.dir = state.localeContext.currentLocale.direction;
+
+                                this.setState(state);
+                            });
+                    });
+            });
+    }
+
+    async getLocales() {
+        const res = await getJsonData(backendURL() + '/locales', { 'X-CSRF-TOKEN': this.state.token })
+        return res;
+    }
+
+    async getCurrentLocale() {
+        const res = await getJsonData(backendURL() + '/locale', { 'X-CSRF-TOKEN': this.state.token })
+        return res;
+    }
+
+
+    async getCurrentTheme() {
+        const res = await getJsonData(backendURL() + '/theme', { 'X-CSRF-TOKEN': this.state.token });
+        return res;
+    }
+
     render() {
         const inputGlobalStyles = <GlobalStyles styles={theme => ({
+            body: {
+                overflowX: 'hidden'
+            },
             '*::-webkit-scrollbar': {
                 width: '0.4em',
             },
@@ -68,6 +142,15 @@ export class App extends Component {
             }
         })} />;
 
+        if (this.state.localeContext.isLocaleLoading || this.state.themeContext.isThemeLoading || this.state.isComponentLoading) {
+            return (
+                <>
+                    {inputGlobalStyles}
+                    < div >!!</div >
+                </>
+            );
+        }
+
         return (
             <LocaleContext.Provider value={this.state.localeContext}>
                 <ThemeContext.Provider value={this.state.themeContext}>
@@ -75,11 +158,11 @@ export class App extends Component {
                         {inputGlobalStyles}
                         <BrowserRouter>
                             <Routes>
-                                <Route path='/' element={<WelcomePage />} />
-                                <Route path='/login' element={<LogInPage />} />
-                                <Route path='/register' element={<SignUpPage />} />
-                                <Route path='/email/verification-notification' element={<EmailVerificationNotice />} />
-                                <Route path='/forgot-password/:redirecturl' element={<ForgotPassword />} />
+                                <Route path='/' element={<WelcomePage currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
+                                <Route path='/login' element={<LogInPage currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
+                                <Route path='/register' element={<SignUpPage currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
+                                <Route path='/email/verification-notification' element={<EmailVerificationNotice currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
+                                <Route path='/forgot-password/:redirecturl' element={<ForgotPassword currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
                             </Routes>
                         </BrowserRouter>
                     </ThemeProvider>
