5416756714a999b6122def7765abd921caf3f6c5
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-09 01:13:50 -0700

    Update SignUpPage react component

--- resources/js/react/components/pages/auth/SignUpPage.js
+++ resources/js/react/components/pages/auth/SignUpPage.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import SignUpHeader from '../../headers/SignUpHeader.js';
 import SignUpForm from '../../../forms/auth/SignUpForm.js';
+import Header from '../../headers/Header.js';
 
 import Grid from '@mui/material/Grid';
+import { Button } from '@mui/material';
+import { translate } from '../../../traslation/translate.js';
 
 export class SignUpPage extends Component {
     render() {
@@ -11,20 +14,27 @@ export class SignUpPage extends Component {
             <>
                 <Grid container spacing={1} >
                     <Grid item xs={12} >
-                        <SignUpHeader />
+                        <Header
+                            title={<Link to='/' style={{ textDecoration: 'none', color: 'white' }} >{translate('general/sign-up/ucFirstLetterAllWords', this.props.currentLocaleName)}</ Link>}
+                            rightSide={
+                                <Button type='button' sx={{ a: { textDecoration: 'none', color: 'white' }, m: 1 }}>
+                                    <Link to="/login" >{translate('general/log-in/ucFirstLetterAllWords', this.props.currentLocaleName)}</Link>
+                                </Button>
+                            }
+                        />
                     </Grid>
                     <Grid item xs={12} >
                         <Grid container >
                             <Grid item xs >
                             </Grid>
                             <Grid item xs={12} sm={9} md={6} >
-                                <SignUpForm />
+                                <SignUpForm currentLocaleName={this.props.currentLocaleName} />
                             </Grid>
                             <Grid item xs >
                             </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item xs={12} >footer
+                    <Grid item xs={12} >
                         {/* <Footer /> */}
                     </Grid>
                 </Grid>
