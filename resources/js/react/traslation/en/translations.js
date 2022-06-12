27bc3a3b4ebec7c1cd03cff5519f5d1f47d0206c
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-12 00:53:40 -0700

    Update translations js module

--- resources/js/react/traslation/en/translations.js
+++ resources/js/react/traslation/en/translations.js
@@ -1,7 +1,9 @@
+import { generalSentences } from './generalSentences.js';
 import { general } from './general.js';
 import { pages } from './pages.js';
 
 let en = {
+    generalSentences: generalSentences,
     general: general,
     pages: pages,
 };
