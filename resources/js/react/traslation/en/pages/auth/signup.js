9dee70e4ccbb00f2eba723fdf9f1f87621dff344
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-12 00:57:24 -0700

    Update signup js module

--- resources/js/react/traslation/en/pages/auth/signup.js
+++ resources/js/react/traslation/en/pages/auth/signup.js
@@ -2,12 +2,10 @@ import { ucFirstLetterAllWords } from '../../../translate.js';
 import { general } from '../../general.js';
 
 let signup = {
-    steps: {
-        'send-phone-number-verification-code': ucFirstLetterAllWords('Send ' + general.phonenumber.single + ' Verification Code'),
-        'fill-registration-form': 'Fill Registration Form',
-        'security-code': 'Security Code',
-        'choose-avatar': ucFirstLetterAllWords('choose an ' + general.avatar.single),
-    }
+    'send-phone-number-verification-code': ucFirstLetterAllWords('Send ' + general.phonenumber.single.ucFirstLetterAllWords + ' Verification Code'),
+    'fill-registration-form': 'Fill Registration Form',
+    'security-code': 'Security Code',
+    'choose-avatar': ucFirstLetterAllWords('choose an ' + general.avatar.single.allLowerCase),
 };
 
 export { signup };
