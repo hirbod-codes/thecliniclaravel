16e6255d3f9603b66b03d9d9b91b1a551b3086b2
Hirbod <hirbod.khatami.words@gmail.com>
2022-06-12 00:49:13 -0700

    Update light js module

--- resources/js/react/themes/light.js
+++ resources/js/react/themes/light.js
@@ -2,17 +2,6 @@ import { createTheme } from '@mui/material/styles';
 
 let theme = {};
 
-// theme = {
-//     palette: {
-//         primary: {
-//             main: '#0052cc',
-//         },
-//         secondary: {
-//             main: '#edf2ff',
-//         },
-//     },
-// }
-
-theme = createTheme();
+theme = createTheme({ palette: { mode: 'light' } });
 
 export default theme;
