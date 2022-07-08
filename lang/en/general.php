f69e1176e1b453fdc9b062eef84029d296eddf1c
Hirbod <hirbod.khatami.words@gmail.com>
2022-07-07 23:56:36 -0700

    Update ./lang/en/general.php translation file

--- lang/en/general.php
+++ lang/en/general.php
@@ -4,4 +4,5 @@ return [
     'theme-store-success' => "{0} Theme successfully changed.",
     'set-locale-success' => "{0} The locale option successfully updated.",
     'set-locale-failure' => "{0} Failed to set provided locale option.",
+    'CSRF-token-mismatch' => "{0} Please Refresh your browser.",
 ];
