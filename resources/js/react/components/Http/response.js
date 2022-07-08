3043c729352faece0f4d51d237cd3dba8561282b
Hirbod <hirbod.khatami.words@gmail.com>
2022-07-07 23:58:45 -0700

    Create response js module

---
+++ resources/js/react/components/Http/response.js
@@ -0,0 +1,44 @@
+import { FormHelperText } from "@mui/material";
+
+function collectMessagesFromResponse(data) {
+    if (typeof data === 'string') {
+        return [data];
+    }
+
+    if (typeof data === 'object') {
+        if (data.hasOwnProperty('errors')) {
+            let messages = [];
+            for (const k in data.errors) {
+                if (data.errors.hasOwnProperty(k)) {
+                    const val = data.errors[k];
+
+                    messages = messages.concat(val);
+                }
+            }
+            return messages;
+        } else {
+            if (data.hasOwnProperty('message')) {
+                return [data.message];
+            } else {
+                return false;
+            }
+        }
+    }
+}
+
+function makeFormHelperTextComponents(data, isError = true) {
+    console.log(data);
+    let components = [];
+    let i = 0;
+    for (const k in data) {
+        if (Object.hasOwnProperty.call(data, k)) {
+            const v = data[k];
+
+            components.push(<FormHelperText key={i} error={isError}>{v}</FormHelperText>);
+        }
+        i++;
+    }
+    return components;
+}
+
+export { collectMessagesFromResponse, makeFormHelperTextComponents }
