857101ba072bc46e7db0075683a0dbb13fae2fc1
Hirbod <hirbod.khatami.words@gmail.com>
2022-07-22 01:27:18 -0700

    Update App.js react component

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -14,20 +14,25 @@ import { LocaleContext } from './components/localeContext.js';
 import WelcomePage from "./components/pages/WelcomePage.js";
 import LogInPage from "./components/pages/auth/LogInPage.js";
 import SignUpPage from "./components/pages/auth/SignUpPage.js";
-import OrderLaserPage from './components/pages/Order/OrderLaserPage';
-import VisitPage from './components/pages/Visit/VisitPage';
 
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
 import { GlobalStyles } from '@mui/material';
-import { backendURL, getJsonData, postJsonData, putJsonData } from './components/Http/fetch';
+import { getJsonData, postJsonData, putJsonData } from './components/Http/fetch';
+import DashboardOrderPage from './components/pages/dashboard/DashboardOrderPage';
+import DashboardVisitPage from './components/pages/dashboard/DashboardVisitPage';
+import UserIconNavigator from './components/UserIconNavigator';
+import { updateState } from './components/helpers';
+import { PrivilegesContext } from './components/privilegesContext';
 
 export class App extends Component {
     constructor(props) {
         super(props);
 
+        this.getDataSynchronously = this.getDataSynchronously.bind(this);
+
         this.changeLocale = (name) => {
-            putJsonData(backendURL() + '/locale', { 'locale': name }, { 'X-CSRF-TOKEN': this.state.token, 'Content-type': '*/*' })
+            putJsonData('/locale', { 'locale': name }, { 'X-CSRF-TOKEN': this.state.token, 'Content-type': '*/*' })
                 .then((res) => {
                     if (res.status === 200) {
                         document.location.reload();
@@ -38,7 +43,7 @@ export class App extends Component {
         };
 
         this.changeTheme = (name) => {
-            postJsonData(backendURL() + '/theme', { theme: name }, { 'X-CSRF-TOKEN': this.state.token })
+            postJsonData('/theme', { theme: name }, { 'X-CSRF-TOKEN': this.state.token })
                 .then((res) => {
                     if (res.status === 200) {
                         document.location.reload();
@@ -50,7 +55,23 @@ export class App extends Component {
 
         this.state = {
             token: document.head.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-            isComponentLoading: true,
+
+            finishStatus: {
+                locales: false,
+                locale: false,
+                themeData: false,
+            },
+
+            isAuthenticationLoading: true,
+            isAuthenticated: false,
+            isAccountLoading: true,
+            account: null,
+            privileges: null,
+
+            isEmailVerified: false,
+            isAvatarLoading: true,
+            image: null,
+
             themeContext: {
                 theme: {},
                 changeTheme: this.changeTheme,
@@ -67,60 +88,111 @@ export class App extends Component {
     }
 
     shouldComponentUpdate() {
-        return !this.state.isComponentLoading;
+        for (const k in this.state.finishStatus) {
+            if (Object.hasOwnProperty.call(this.state.finishStatus, k)) {
+                const v = this.state.finishStatus[k];
+
+                if (!v) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     componentDidMount() {
-        let state = this.state;
+        this.getDataSynchronously();
+    }
 
-        this.getLocales()
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                state.localeContext.locales = data;
+    // For perfomance reasons we are not using asynchronous programming for this method
+    getDataSynchronously() {
+        getJsonData('/isAuthenticated', { 'X-CSRF-TOKEN': this.state.token }).then((res) => res.json())
+            .then((isAuthenticated) => {
+                updateState(this, (state) => {
+                    state.isAuthenticationLoading = false;
+                    state.isAuthenticated = isAuthenticated.authenticated;
+                    return state;
+                });
+            });
 
-                this.getCurrentLocale()
-                    .then((res) => {
-                        return res.json();
-                    })
-                    .then((data) => {
-                        state.localeContext.currentLocale = data;
-                        state.localeContext.isLocaleLoading = false;
+        getJsonData('/account', { 'X-CSRF-TOKEN': this.state.token }).then((res) => { return res.status === 200 ? res.json() : null })
+            .then((account) => {
+                if (account !== null) {
+                    updateState(this, (state) => {
+                        state.isAccountLoading = false;
+                        state.account = account;
+                        return state;
+                    });
 
-                        this.getCurrentTheme()
-                            .then((res) => {
-                                return res.json();
-                            })
-                            .then((data) => {
-                                state.themeContext.theme = createTheme(resolveTheme(data.theme + '-' + state.localeContext.currentLocale.direction));
-                                state.themeContext.currentTheme = data.theme;
-                                state.themeContext.isThemeLoading = false;
+                    getJsonData('/avatar?accountId=' + account.id, { 'X-CSRF-TOKEN': this.state.token }).then((res) => { return res.status === 200 ? res.text() : null })
+                        .then((avatar) => {
+                            if (avatar !== null) {
+                                updateState(this, (state) => {
+                                    state.isAvatarLoading = false;
+                                    state.image = 'data:image/png;base64,' + avatar;
+                                    return state;
+                                });
+                            }
+                        });
 
-                                state.isComponentLoading = false;
+                    getJsonData('/privileges/show?accountId=' + account.id, { 'X-CSRF-TOKEN': this.state.token }).then((res) => { return res.status === 200 ? res.json() : null })
+                        .then((privileges) => {
+                            if (privileges !== null) {
+                                updateState(this, (state) => {
+                                    state.privileges = privileges;
+                                    return state;
+                                });
+                            }
+                        });
+                } else {
+                    updateState(this, (state) => {
+                        state.isAccountLoading = false;
+                        return state;
+                    });
+                }
+            });
 
-                                document.dir = state.localeContext.currentLocale.direction;
+        getJsonData('/isEmailVerified', { 'X-CSRF-TOKEN': this.state.token }).then((res) => { return res.status === 200 ? res.json() : null })
+            .then((isEmailVerified) => {
+                if (isEmailVerified !== null) {
+                    updateState(this, (state) => {
+                        state.isEmailVerified = isEmailVerified.verified;
+                        return state;
+                    });
+                }
+            });
 
-                                this.setState(state);
-                            });
+        getJsonData('/locale', { 'X-CSRF-TOKEN': this.state.token }).then((res) => res.json())
+            .then((locale) => {
+                updateState(this, (state) => {
+                    state.finishStatus.locale = true;
+                    state.localeContext.currentLocale = locale;
+                    state.localeContext.isLocaleLoading = false;
+                    return state;
+                });
+                document.dir = locale.direction;
+
+                getJsonData('/theme', { 'X-CSRF-TOKEN': this.state.token }).then((res) => res.json())
+                    .then((themeData) => {
+                        updateState(this, (state) => {
+                            state.finishStatus.themeData = true;
+                            state.themeContext.theme = createTheme(resolveTheme(themeData.theme + '-' + locale.direction));
+                            state.themeContext.currentTheme = themeData.theme;
+                            state.themeContext.isThemeLoading = false;
+                            return state;
+                        });
+                        document.dir = locale.direction;
                     });
             });
-    }
 
-    async getLocales() {
-        const res = await getJsonData(backendURL() + '/locales', { 'X-CSRF-TOKEN': this.state.token })
-        return res;
-    }
-
-    async getCurrentLocale() {
-        const res = await getJsonData(backendURL() + '/locale', { 'X-CSRF-TOKEN': this.state.token })
-        return res;
-    }
-
-
-    async getCurrentTheme() {
-        const res = await getJsonData(backendURL() + '/theme', { 'X-CSRF-TOKEN': this.state.token });
-        return res;
+        getJsonData('/locales', { 'X-CSRF-TOKEN': this.state.token }).then((res) => res.json())
+            .then((locales) => {
+                updateState(this, (state) => {
+                    state.finishStatus.locales = true;
+                    state.localeContext.locales = locales;
+                    return state;
+                });
+            });
     }
 
     render() {
@@ -140,35 +212,46 @@ export class App extends Component {
             }
         })} />;
 
-        if (this.state.localeContext.isLocaleLoading || this.state.themeContext.isThemeLoading || this.state.isComponentLoading) {
-            return (
-                <>
-                    {inputGlobalStyles}
-                    < div >!!</div >
-                </>
-            );
+        let authProps = {
+            isAuthenticationLoading: this.state.isAuthenticationLoading,
+            isAuthenticated: this.state.isAuthenticated,
+            account: this.state.account,
+            privileges: this.state.privileges,
+        };
+
+        let navigator = null;
+        let currentLocaleName = null;
+        if (!this.state.localeContext.isLocaleLoading && !this.state.themeContext.isThemeLoading) {
+            currentLocaleName = this.state.localeContext.currentLocale.shortName;
+            navigator = <UserIconNavigator image={this.state.image} isAvatarLoading={this.state.isAvatarLoading} isEmailVerified={this.state.isEmailVerified} currentLocaleName={currentLocaleName} />;
         }
 
         return (
-            <LocaleContext.Provider value={this.state.localeContext}>
-                <ThemeContext.Provider value={this.state.themeContext}>
-                    <ThemeProvider theme={this.state.themeContext.theme}>
-                        {inputGlobalStyles}
-                        <BrowserRouter>
-                            <Routes>
-                                <Route path='/' element={<WelcomePage currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
-                                <Route path='/login' element={<LogInPage currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
-                                <Route path='/register' element={<SignUpPage currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
+            this.state.localeContext.isLocaleLoading || this.state.themeContext.isThemeLoading || this.state.isAccountLoading ?
+                <>
+                    {inputGlobalStyles}
+                    < div ></div >
+                </> :
+                <LocaleContext.Provider value={this.state.localeContext}>
+                    <ThemeContext.Provider value={this.state.themeContext}>
+                        <ThemeProvider theme={this.state.themeContext.theme}>
+                            <PrivilegesContext.Provider value={this.state.privileges}>
+                                {inputGlobalStyles}
+                                <BrowserRouter>
+                                    <Routes>
+                                        <Route path='/' element={<WelcomePage navigator={navigator} currentLocaleName={currentLocaleName} {...authProps} />} />
+                                        <Route path='/login' element={<LogInPage navigator={navigator} currentLocaleName={currentLocaleName} {...authProps} />} />
+                                        <Route path='/register' element={<SignUpPage navigator={navigator} currentLocaleName={currentLocaleName} {...authProps} />} />
 
-                                <Route path='/order/laser/page' element={<OrderLaserPage currentLocaleDirection={this.state.localeContext.currentLocale.direction} currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
-                                <Route path='/visit/laser/page' element={<VisitPage businessName='laser' currentLocaleDirection={this.state.localeContext.currentLocale.direction} currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
-                                <Route path='/visit/regular/page' element={<VisitPage businessName='regular' currentLocaleDirection={this.state.localeContext.currentLocale.direction} currentLocaleName={this.state.localeContext.currentLocale.shortName} />} />
-                            </Routes>
-                        </BrowserRouter>
-                    </ThemeProvider>
-                </ThemeContext.Provider>
-            </LocaleContext.Provider>
-        )
+                                        <Route path='/dashboard/visit' element={<DashboardVisitPage navigator={navigator} currentLocaleName={currentLocaleName} {...authProps} />} />
+                                        <Route path='/dashboard/order' element={<DashboardOrderPage navigator={navigator} currentLocaleName={currentLocaleName} {...authProps} />} />
+                                    </Routes>
+                                </BrowserRouter>
+                            </PrivilegesContext.Provider>
+                        </ThemeProvider>
+                    </ThemeContext.Provider>
+                </LocaleContext.Provider>
+        );
     }
 }
 
