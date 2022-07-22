f65822ee5237da7a0cbb1eb539f24eed992aeb49
Hirbod <hirbod.khatami.words@gmail.com>
2022-07-22 01:15:17 -0700

    Create ShowAllRequest class

---
+++ app/Http/Requests/Roles/ShowAllRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Roles;
+
+use App\Rules\ProhibitExtraFeilds;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShowAllRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $array = [
+            'accountId' => ['required', 'string', 'integer', 'numeric', 'min:0'],
+        ];
+
+        array_unshift($array[array_key_first($array)], new ProhibitExtraFeilds($array));
+
+        return $array;
+    }
+}
