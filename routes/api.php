da20b7f0042fbaef520eb8d3aa3fdb58c5264cdf
Hirbod <hirbod.khatami.words@gmail.com>
2022-07-26 05:53:26 -0700

    Add /account/verify-phoennumber-verification-code route to ./routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -114,6 +114,7 @@ Route::prefix('{locale}')->group(function () {
 
                 // Phonenumber Verification Message Sender Route
                 Route::post('/account/send-phoennumber-verification-code', 'sendPhonenumberVerificationCode')->name('account.sendPhonenumberVerificationCode');
+                Route::post('/account/verify-phoennumber-verification-code', 'isPhonenumberVerificationCodeVerified')->name('auth.isPhonenumberVerificationCodeVerified');
 
                 Route::post('/account/{roleName}', 'store')->name('account.store');
 
