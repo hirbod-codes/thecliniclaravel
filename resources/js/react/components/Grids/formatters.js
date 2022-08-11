df23de815527daf941393439cc8d76e7d0364ef5
Hirbod <hirbod.khatami.words@gmail.com>
2022-08-11 00:27:42 -0700

    Update formatters.js js module

--- resources/js/react/components/Grids/formatters.js
+++ resources/js/react/components/Grids/formatters.js
@@ -1,17 +1,98 @@
-function formatToNumber(props) {
-    if (props.value === undefined) {
+import { translate } from "../../traslation/translate";
+import { localizeDate } from "../helpers";
+import { LocaleContext } from "../localeContext";
+
+function formatToNumber(params) {
+    if (params.value === undefined) {
         return '';
     }
 
-    return Number(props.value);
+    return Number(params.value);
 }
 
-function formatToTime(props) {
-    if (props.value === undefined) {
+function formatToTime(params) {
+    if (params.value === undefined) {
         return '';
     }
 
-    return Math.floor(props.value / 3600) + ':' + Math.floor((props.value % 3600) / 60) + ':' + Math.floor((props.value % 3600) % 60);
+    let date = null;
+    switch (params.field) {
+        case 'valueconsumingTime ':
+            return Math.floor(params.value / 3600) + ':' + Math.floor((params.value % 3600) / 60) + ':' + Math.floor((params.value % 3600) % 60);
+
+        case 'visitTimestamp':
+            return Math.floor(params.value / 3600) + ':' + Math.floor((params.value % 3600) / 60) + ':' + Math.floor((params.value % 3600) % 60);
+
+        case 'neededTime':
+            return Math.floor(params.value / 3600) + ':' + Math.floor((params.value % 3600) / 60) + ':' + Math.floor((params.value % 3600) % 60);
+
+        default:
+            const locale = LocaleContext._currentValue.currentLocale.shortName;
+            date = localizeDate('utc', params.value, locale, true);
+            break;
+    }
+
+
+    return date;
 }
 
-export { formatToNumber, formatToTime };
+function foramtColumnsFromBackEnd(columns) {
+    let formattedColumns = [];
+    let found = false;
+    for (let i = 0; i < columns.length; i++) {
+        found = false;
+        for (let j = 0; j < formattedColumns.length; j++) {
+            if (formattedColumns[j].field === columns[i].name) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            continue;
+        }
+
+        let type = formatColumnTypeFromBackEnd(columns[i].type);
+        let headerName = '';
+        try {
+            headerName = translate('general/columns/' + columns[i].name + '/single/ucFirstLetterFirstWord');
+        } catch (error) {
+            try {
+                headerName = translate('general/columns/account/' + columns[i].name + '/single/ucFirstLetterFirstWord');
+            } catch (error1) {
+                continue;
+            }
+        }
+
+        let column = {
+            field: columns[i].name,
+            headerName: headerName,
+            description: headerName,
+            type: type,
+            valueFormatter: type === 'number' ? formatToNumber : (type === 'dateTime' ? formatToTime : (params) => params.value),
+            minWidth: type === 'dateTime' ? 190 : 150,
+        };
+
+        formattedColumns.push(column);
+    }
+
+    return formattedColumns;
+}
+
+function formatColumnTypeFromBackEnd(type) {
+    switch (type) {
+        case 'integer':
+            return 'number';
+
+        case 'bigint':
+            return 'number';
+
+        case 'datetime':
+            return 'dateTime';
+
+        default:
+            return type;
+    }
+}
+
+export { formatColumnTypeFromBackEnd, foramtColumnsFromBackEnd, formatToNumber, formatToTime };
