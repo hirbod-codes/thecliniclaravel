a20304b0f9255c2dfaa9c366db6a19571ad3d23e
Hirbod <hirbod.khatami.words@gmail.com>
2022-08-16 12:12:27 -0700

    Update DatabasePrivilegeNameSeeder seeder class

--- database/seeders/DatabasePrivilegeNameSeeder.php
+++ database/seeders/DatabasePrivilegeNameSeeder.php
@@ -9,6 +9,9 @@ class DatabasePrivilegeNameSeeder extends Seeder
 {
     public function run(): void
     {
+
         (new PrivilegeName(['name' => 'editAvatar']))->saveOrFail();
+        (new PrivilegeName(['name' => 'editRegularOrderPrice']))->saveOrFail();
+        (new PrivilegeName(['name' => 'editRegularOrderNeededTime']))->saveOrFail();
     }
 }
