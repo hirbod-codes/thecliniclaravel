3b43ba525e7ecd0d42c478543310c0b5af1d178b
Hirbod <hirbod.khatami.words@gmail.com>
2022-08-16 03:26:55 -0700

    Update App react component

--- resources/js/react/App.js
+++ resources/js/react/App.js
@@ -12,8 +12,13 @@ import CloseIcon from '@mui/icons-material/Close';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
 import { Alert, GlobalStyles, IconButton, Snackbar } from '@mui/material';
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
 
-import { ThemeContext, resolveTheme } from './components/themeContenxt.js';
+
+import { ThemeContext, resolveTheme, resolveLocalization } from './components/themeContenxt.js';
 import { LocaleContext } from './components/localeContext.js';
 
 import WelcomePage from "./components/pages/WelcomePage.js";
@@ -28,13 +33,18 @@ import UserIconNavigator from './components/UserIconNavigator';
 import { updateState } from './components/helpers';
 import { formatPrivileges, PrivilegesContext } from './components/privilegesContext';
 
-export class App extends Component {
+class App extends Component {
     constructor(props) {
         super(props);
 
         this.getDataSynchronously = this.getDataSynchronously.bind(this);
 
         this.handleFeedbackClose = this.handleFeedbackClose.bind(this);
+
+        this.onLogout = this.onLogout.bind(this);
+        this.onLogin = this.onLogin.bind(this);
+        this.onRegister = this.onRegister.bind(this);
+        this.resetAuthInfo = this.resetAuthInfo.bind(this);
 
         this.changeLocale = async (name) => {
             let r = await fetchData('put', '/locale', { 'locale': name }, { 'X-CSRF-TOKEN': this.state.token, 'Content-type': '*/*' });
@@ -114,82 +124,110 @@ export class App extends Component {
         this.setState({ feedbackMessages: feedbackMessages });
     }
 
-    // For perfomance reasons we are not using asynchronous programming for this method
     getDataSynchronously() {
-        fetchData('get', '/isAuthenticated', {}, { 'X-CSRF-TOKEN': this.state.token }).then((isAuthenticatedResponse) => {
-            if (isAuthenticatedResponse.response.status !== 200) {
-                return;
-            }
-
-            updateState(this, { isAuthenticationLoading: false, isAuthenticated: isAuthenticatedResponse.value.authenticated });
-
-            fetchData('get', '/account', {}, { 'X-CSRF-TOKEN': this.state.token }).then((accountResponse) => {
-                updateState(this, { isAccountLoading: false });
-
-                if (accountResponse.response.status !== 200) {
+        return new Promise((resolve) => {
+            fetchData('get', '/isAuthenticated', {}, { 'X-CSRF-TOKEN': this.state.token }).then((isAuthenticatedResponse) => {
+                if (isAuthenticatedResponse.response.status !== 200 || isAuthenticatedResponse.value.authenticated !== true) {
                     return;
                 }
-                updateState(this, { account: accountResponse.value });
 
-                fetchData('get', '/avatar?accountId=' + accountResponse.value.id, {}, { 'X-CSRF-TOKEN': this.state.token }).then((avatarResponse) => {
-                    if (avatarResponse.response.status === 200) {
-                        updateState(this, { isAvatarLoading: false, image: 'data:image/png;base64,' + avatarResponse.value });
+                updateState(this, { isAuthenticationLoading: false, isAuthenticated: isAuthenticatedResponse.value.authenticated });
+
+                fetchData('get', '/account', {}, { 'X-CSRF-TOKEN': this.state.token }).then((accountResponse) => {
+                    updateState(this, { isAccountLoading: false });
+
+                    if (accountResponse.response.status !== 200) {
+                        return;
+                    }
+                    updateState(this, { account: accountResponse.value });
+
+                    fetchData('get', '/avatar?accountId=' + accountResponse.value.id, {}, { 'X-CSRF-TOKEN': this.state.token }).then((avatarResponse) => {
+                        if (avatarResponse.response.status === 200) {
+                            updateState(this, { isAvatarLoading: false, image: 'data:image/png;base64,' + avatarResponse.value });
+                        }
+                    });
+                });
+
+                fetchData('get', '/role', {}, { 'X-CSRF-TOKEN': this.state.token }).then((privilegesResponse) => {
+                    if (privilegesResponse.response.status === 200) {
+                        updateState(this, { privileges: privilegesResponse.value, arePrivilegesLoading: false });
+                    }
+                    updateState(this, { arePrivilegesLoading: false });
+                });
+
+                fetchData('get', '/isEmailVerified', {}, { 'X-CSRF-TOKEN': this.state.token }).then((emailResponse) => {
+                    if (emailResponse.response.status === 200) {
+                        updateState(this, { isEmailVerified: emailResponse.value });
                     }
                 });
             });
 
-            fetchData('get', '/role', {}, { 'X-CSRF-TOKEN': this.state.token }).then((privilegesResponse) => {
-                if (privilegesResponse.response.status === 200) {
-                    updateState(this, { privileges: privilegesResponse.value, arePrivilegesLoading: false });
+            fetchData('get', '/locale', {}, { 'X-CSRF-TOKEN': this.state.token }).then((localeResponse) => {
+                let locale = localeResponse.value;
+                if (localeResponse.response.status !== 200) {
+                    return;
                 }
-                updateState(this, { arePrivilegesLoading: false });
+                updateState(this, (state) => {
+                    state.finishStatus.locale = true;
+                    state.localeContext.currentLocale = locale;
+                    state.localeContext.isLocaleLoading = false;
+                    return state;
+                });
+
+                document.dir = locale.direction;
+                document.body.setAttribute('dir', locale.direction);
+
+                fetchData('get', '/theme', {}, { 'X-CSRF-TOKEN': this.state.token }).then((themeResponse) => {
+                    let themeData = themeResponse.value;
+                    if (themeResponse.response.status === 200) {
+                        updateState(this, (state) => {
+                            state.finishStatus.themeData = true;
+                            state.themeContext.theme = createTheme(resolveTheme(themeData.theme + '-' + locale.direction), resolveLocalization(locale.shortName));
+                            state.themeContext.currentTheme = themeData.theme;
+                            state.themeContext.isThemeLoading = false;
+                            return state;
+                        });
+                    }
+                });
             });
-        });
 
-        fetchData('get', '/isEmailVerified', {}, { 'X-CSRF-TOKEN': this.state.token }).then((emailResponse) => {
-            if (emailResponse.response.status === 200) {
-                updateState(this, { isEmailVerified: emailResponse.value });
-            }
-        });
-
-        fetchData('get', '/locale', {}, { 'X-CSRF-TOKEN': this.state.token }).then((localeResponse) => {
-            let locale = localeResponse.value;
-            if (localeResponse.response.status !== 200) {
-                return;
-            }
-            updateState(this, (state) => {
-                state.finishStatus.locale = true;
-                state.localeContext.currentLocale = locale;
-                state.localeContext.isLocaleLoading = false;
-                return state;
-            });
-
-            document.dir = locale.direction;
-
-            fetchData('get', '/theme', {}, { 'X-CSRF-TOKEN': this.state.token }).then((themeResponse) => {
-                let themeData = themeResponse.value;
-                if (themeResponse.response.status === 200) {
+            fetchData('get', '/locales', {}, { 'X-CSRF-TOKEN': this.state.token }).then((localesResponse) => {
+                let locales = localesResponse.value;
+                if (localesResponse.response.status === 200) {
                     updateState(this, (state) => {
-                        state.finishStatus.themeData = true;
-                        state.themeContext.theme = createTheme(resolveTheme(themeData.theme + '-' + locale.direction));
-                        state.themeContext.currentTheme = themeData.theme;
-                        state.themeContext.isThemeLoading = false;
+                        state.finishStatus.locales = true;
+                        state.localeContext.locales = locales;
                         return state;
                     });
                 }
             });
-        });
 
-        fetchData('get', '/locales', {}, { 'X-CSRF-TOKEN': this.state.token }).then((localesResponse) => {
-            let locales = localesResponse.value;
-            if (localesResponse.response.status === 200) {
-                updateState(this, (state) => {
-                    state.finishStatus.locales = true;
-                    state.localeContext.locales = locales;
-                    return state;
-                });
-            }
+            resolve();
         });
+    }
+
+    onLogout() {
+        this.resetAuthInfo()
+    }
+
+    resetAuthInfo() {
+        this.setState({
+            isAuthenticated: false,
+            account: null,
+            privileges: null,
+            isEmailVerified: null,
+            image: null,
+        });
+    }
+
+    async onLogin() {
+        this.resetAuthInfo();
+        await this.getDataSynchronously();
+    }
+
+    async onRegister() {
+        this.resetAuthInfo();
+        await this.getDataSynchronously();
     }
 
     render() {
@@ -212,6 +250,7 @@ export class App extends Component {
         let authProps = {
             isAuthenticationLoading: this.state.isAuthenticationLoading,
             isAuthenticated: this.state.isAuthenticated,
+            onLogout: this.onLogout,
             account: this.state.account,
             privileges: this.state.privileges,
         };
@@ -221,54 +260,60 @@ export class App extends Component {
             navigator = <UserIconNavigator image={this.state.image} isAvatarLoading={this.state.isAvatarLoading} isEmailVerified={this.state.isEmailVerified} />;
         }
 
+        const cacheLtr = createCache({ key: "muiltr" });
 
+        const cacheRtl = createCache({ key: "muirtl", stylisPlugins: [prefixer, rtlPlugin] });
+
+        console.log(this.state);
         return (
-            this.state.localeContext.isLocaleLoading || this.state.themeContext.isThemeLoading || this.state.isAccountLoading || this.state.arePrivilegesLoading ?
+            (this.state.localeContext.isLocaleLoading || this.state.themeContext.isThemeLoading || (this.state.isAuthenticated && (this.state.isAccountLoading || this.state.arePrivilegesLoading))) ?
                 <>
                     {inputGlobalStyles}
-                    < div ></div >
+                    < div >!!!</div >
                 </> :
-                <LocaleContext.Provider value={this.state.localeContext}>
-                    <ThemeContext.Provider value={this.state.themeContext}>
-                        <ThemeProvider theme={this.state.themeContext.theme}>
-                            <PrivilegesContext.Provider value={formatPrivileges(this.state.privileges)}>
-                                {inputGlobalStyles}
-                                <BrowserRouter>
-                                    <Routes>
-                                        <Route path='/' element={<WelcomePage navigator={navigator} {...authProps} />} />
-                                        <Route path='/login' element={<LogInPage navigator={navigator} {...authProps} />} />
-                                        <Route path='/register' element={<SignUpPage navigator={navigator} {...authProps} />} />
+                <CacheProvider value={this.state.localeContext.currentLocale.direction === 'rtl' ? cacheRtl : cacheLtr}>
+                    <LocaleContext.Provider value={this.state.localeContext}>
+                        <ThemeContext.Provider value={this.state.themeContext}>
+                            <ThemeProvider theme={this.state.themeContext.theme}>
+                                <PrivilegesContext.Provider value={formatPrivileges(this.state.privileges)}>
+                                    {inputGlobalStyles}
+                                    <BrowserRouter>
+                                        <Routes>
+                                            <Route path='/' element={<WelcomePage navigator={navigator} {...authProps} />} />
+                                            <Route path='/login' element={<LogInPage navigator={navigator} {...authProps} onLogin={this.onLogin} />} />
+                                            <Route path='/register' element={<SignUpPage navigator={navigator} {...authProps} onRegister={this.onRegister} />} />
 
-                                        <Route path='/dashboard/account' element={<DashboardAccountPage navigator={navigator} {...authProps} />} />
-                                        <Route path='/dashboard/order' element={<DashboardOrderPage navigator={navigator} {...authProps} />} />
-                                        <Route path='/dashboard/visit' element={<DashboardVisitPage navigator={navigator} {...authProps} />} />
-                                    </Routes>
-                                </BrowserRouter>
+                                            <Route path='/dashboard/account' element={<DashboardAccountPage navigator={navigator} {...authProps} />} />
+                                            <Route path='/dashboard/order' element={<DashboardOrderPage navigator={navigator} {...authProps} />} />
+                                            <Route path='/dashboard/visit' element={<DashboardVisitPage navigator={navigator} {...authProps} />} />
+                                        </Routes>
+                                    </BrowserRouter>
 
-                                {this.state.feedbackMessages.map((m, i) =>
-                                    <Snackbar
-                                        key={i}
-                                        open={m.open}
-                                        autoHideDuration={6000}
-                                        onClose={(e, r) => this.handleFeedbackClose(e, r, i)}
-                                        action={
-                                            <IconButton
-                                                size="small"
-                                                onClick={(e, r) => this.handleFeedbackClose(e, r, i)}
-                                            >
-                                                <CloseIcon fontSize="small" />
-                                            </IconButton>
-                                        }
-                                    >
-                                        <Alert onClose={(e, r) => this.handleFeedbackClose(e, r, i)} severity={m.color} sx={{ width: '100%' }}>
-                                            {m.message}
-                                        </Alert>
-                                    </Snackbar>
-                                )}
-                            </PrivilegesContext.Provider>
-                        </ThemeProvider>
-                    </ThemeContext.Provider>
-                </LocaleContext.Provider>
+                                    {this.state.feedbackMessages.map((m, i) =>
+                                        <Snackbar
+                                            key={i}
+                                            open={m.open}
+                                            autoHideDuration={6000}
+                                            onClose={(e, r) => this.handleFeedbackClose(e, r, i)}
+                                            action={
+                                                <IconButton
+                                                    size="small"
+                                                    onClick={(e, r) => this.handleFeedbackClose(e, r, i)}
+                                                >
+                                                    <CloseIcon fontSize="small" />
+                                                </IconButton>
+                                            }
+                                        >
+                                            <Alert onClose={(e, r) => this.handleFeedbackClose(e, r, i)} severity={m.color} sx={{ width: '100%' }}>
+                                                {m.message}
+                                            </Alert>
+                                        </Snackbar>
+                                    )}
+                                </PrivilegesContext.Provider>
+                            </ThemeProvider>
+                        </ThemeContext.Provider>
+                    </LocaleContext.Provider>
+                </CacheProvider>
         );
     }
 }
