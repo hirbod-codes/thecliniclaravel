ec4a80a316377e400e22a1b2cff2eab78f486f83
Hirbod <hirbod.khatami.words@gmail.com>
2022-08-20 01:42:24 -0700

    Update ./resources/views/app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="/js/app.js" defer></script>
+
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title>{{ $title ?? env('APP_NAME', 'Document') }}</title>
 </head>
