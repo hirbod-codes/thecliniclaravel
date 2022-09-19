8dba1be380c8e12dee5a271bfe9d000551930433
Hirbod <hirbod.khatami.words@gmail.com>
2022-09-19 08:56:42 -0700

    Create deploy.sh bash script

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+echo "deploying" >> /home/hirbod/deploy.log
+
+Help() {
+    echo "This bash script will deploy an application called the_app in docker swarm."
+    echo
+    echo "Syntax: scriptTemplate [-f1|f2|h|]"
+    echo "options:"
+    echo "h         help."
+    echo
+}
+
+while getopts "ha:s:f:" flag; do
+    case "${flag}" in
+    h)
+        Help
+        exit
+        ;;
+
+    a)
+        a=${OPTARG}
+        echo "a value=$a, ${OPTARG}" >> /home/hirbod/deploy.log
+        ;;
+
+    f)
+        f1=${OPTARG}
+        echo "f value=$f1, ${OPTARG}" >> /home/hirbod/deploy.log
+        ;;
+
+    s)
+        f2=${OPTARG}
+        echo "s value=$f2, ${OPTARG}" >> /home/hirbod/deploy.log
+        ;;
+
+    \?)
+        echo "Invalid option !!!"
+        exit
+        ;;
+
+    esac
+done
+
+docker stack deploy -c /home/hirbod/docker-compose.stack.yml the_app
+
+echo "deployed" >> /home/hirbod/deploy.log
